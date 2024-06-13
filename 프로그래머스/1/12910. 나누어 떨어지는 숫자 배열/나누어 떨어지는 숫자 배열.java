c015d2128a9b78cba1b62b89bb77ce2cad40400c
beomkim7 <152937397+beomkim7@users.noreply.github.com>
2024-06-13 15:45:21 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.56 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -1,31 +1,28 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+
 
 class Solution {
     public int[] solution(int[] arr, int divisor) {
-        ArrayList<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(arr);
         
-        // 주어진 배열을 순회하며 나누어 떨어지는 값을 리스트에 추가
-        for(int num : arr) {
-            if(num % divisor == 0) {
+        for(int num : arr){
+            if(num % divisor == 0 ){
                 list.add(num);
             }
         }
         
-        // 나누어 떨어지는 값이 없으면 -1을 반환
-        if (list.isEmpty()) {
-            return new int[]{-1};
+        int listSize = list.size();
+        
+        if(listSize == 0){
+            int[] answer = {-1};
+            return answer;
         }
         
-        // 리스트를 정렬
-        Collections.sort(list);
-        
-        // 리스트를 배열로 변환하여 반환
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+        int[] answer = new int[listSize];
+        for(int i = 0 ; i < listSize ; i++){
+            answer[i]=list.get(i);            
         }
-        
         return answer;
     }
 }
