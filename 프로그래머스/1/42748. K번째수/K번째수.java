f784ec14942735374e0da5d6696f6d92c2b86375
beomkim7 <152937397+beomkim7@users.noreply.github.com>
2024-06-14 13:29:26 +0900

    [level 1] Title: K번째수, Time: 0.34 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,25 +1,14 @@
-import java.util.List;
-import java.util.Collections;
 import java.util.Arrays;
-import java.util.ArrayList;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
         for(int i = 0 ; i < commands.length ; i++){
-            
             int[] rtArray = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
-            List<Integer> temp = new ArrayList<>();
-            for(int num : rtArray){
-                temp.add(num);
-            }
-            Collections.sort(temp);
-            
-            answer[i] = temp.get(commands[i][2]-1);
-            
+            Arrays.sort(rtArray);
+            answer[i] = rtArray[commands[i][2]-1];
         }
-        
-        
         
         return answer;
     }
