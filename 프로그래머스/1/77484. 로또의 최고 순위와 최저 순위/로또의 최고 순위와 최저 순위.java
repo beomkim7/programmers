a550c7a0ff31dcbf9e3cd3b02d6442c11e6c1269
beomkim7 <152937397+beomkim7@users.noreply.github.com>
2024-06-17 10:30:18 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,34 +1,45 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 class Solution {
     public List<Integer> solution(int[] lottos, int[] win_nums) {
+        Set<Integer> count = new HashSet<>();
         
-        int match = 7;
-        int luck = 7;
+        for(int num : win_nums){
+            count.add(num);
+        }
+        
+        int match = 0;
+        int zero = 0;
+        
+        for(int lotto : lottos){
+            if(lotto == 0){
+                zero++;
+            }
+            if(count.contains(lotto)){
+                match++;
+            }
+        }
+        
+        int finalZero = realNum(zero+match);
+        int finalmatch = realNum(match);
+        
         List<Integer> answer = new ArrayList<>();
-        
-        for(int i = 0 ; i < lottos.length ; i++){
-            for(int j =0 ; j < win_nums.length ; j++){
-                if(lottos[i] == win_nums[j]){
-                    match--;
-                    luck--;
-                    continue;
-                }
-            }
-            if(lottos[i] == 0){
-                luck--;
-            }
-        }
-        
-        if(match == 7){
-            match = 6;
-        }
-        if(luck == 7){
-            luck = 6;
-        }
-        answer.add(luck);
-        answer.add(match);
+        answer.add(finalZero);
+        answer.add(finalmatch);
         
         return answer;
     }
+    private int realNum(int number){
+        switch(number){
+            case 6 : return 1;
+            case 5 : return 2;
+            case 4 : return 3;
+            case 3 : return 4;
+            case 2 : return 5;
+            default: return 6;
+        }
+    }
 }
+
