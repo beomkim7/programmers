e4c46cc73ec9f5b96c1e1483b0646c065531c90c
beomkim7 <152937397+beomkim7@users.noreply.github.com>
2024-06-19 08:53:56 +0900

    [level 1] Title: 문자열 나누기, Time: 0.50 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,28 +1,28 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        char spell = '\0';
+        int count = 0 ;
         
-        String [] spell = s.split("");
-        int i = 0 ;
-        int check = 0 ;
-        int spellCount = 0;
-        while(i<spell.length){
-            check = 1 ;
-            spellCount = 1 ;
-            for(int j = i+1 ; j < spell.length ; j++){
-                if(spell[i].equals(spell[j]))check++;
-                else check--;
-                
-                if(check == 0){
-                    if(i+1 != spell.length){
-                        spellCount++;
-                    }
-                    break;
-                }
-                spellCount++;
+        for(char ch : s.toCharArray()){
+            if(count == 0){
+                spell = ch;
             }
+            
+            if(spell == ch){
+                count++;
+            }else{
+                count--;
+            }
+            
+            if(count == 0){
+                answer++;
+            }
+            
+        }
+        
+        if(count > 0){
             answer++;
-            i +=spellCount;
         }
         
         return answer;
