42fcf8a6b8be2c5be1b1d7b7ec5603e79a66f4a7
beomkim7 <152937397+beomkim7@users.noreply.github.com>
2024-06-19 09:22:57 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.08 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,36 +1,30 @@
-import java.util.*;
+import java.util.HashMap;
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
-        int[] answer = new int[targets.length];
-        HashMap <Character,Integer>map = new HashMap<>();
+        int[] answer = new int [targets.length];
         
-        for(String key : keymap){
-            for(int i = 0 ; i < key.length() ; i++){
-                char ch = key.charAt(i);
-                
-                if(map.containsKey(ch)){
-                    if(map.get(ch)>i){
-                        map.replace(ch,i+1);
-                    }
-                }else{
-                    map.put(ch,i+1);
+        HashMap<Character,Integer> map = new HashMap<>();
+        
+        for(int i = 0 ; i < keymap.length ; i++){
+            for(char ch : keymap[i].toCharArray()){
+                int min = keymap[i].indexOf(ch)+1;
+                if(!map.containsKey(ch) || map.get(ch)>min){
+                    map.put(ch,min);                
                 }
-                
-            }           
+            }
         }
+        
         for(int i = 0 ; i < targets.length ; i++){
-            int sum = 0 ;
-            for(int j = 0 ; j < targets[i].length() ; j++){
-                char ch = targets[i].charAt(j);
-                
+            int num = 0;
+            for(char ch : targets[i].toCharArray()){
                 if(map.containsKey(ch)){
-                    sum +=map.get(ch);
+                    num +=map.get(ch);
                 }else{
-                    sum = -1;
+                    num = -1;
                     break;
                 }
             }
-            answer[i] +=sum;
+            answer[i] +=num;
         }
         
         return answer;
