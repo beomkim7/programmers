fbcf4c3d7b004e6983cc25b2e519f5208cbc0022
beomkim7 <152937397+beomkim7@users.noreply.github.com>
2024-06-19 10:03:01 +0900

    [level 1] Title: 대충 만든 자판, Time: 15.19 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,30 +1,30 @@
-import java.util.HashMap;
+import java.util.Set;
+import java.util.Arrays;
+import java.util.HashSet;
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int [targets.length];
         
-        HashMap<Character,Integer> map = new HashMap<>();
-        
-        for(int i = 0 ; i < keymap.length ; i++){
-            for(char ch : keymap[i].toCharArray()){
-                int min = keymap[i].indexOf(ch)+1;
-                if(!map.containsKey(ch) || map.get(ch)>min){
-                    map.put(ch,min);                
-                }
-            }
-        }
+        Set <String> spell = new HashSet<>(Arrays.asList(keymap));
+        spell.remove("");
         
         for(int i = 0 ; i < targets.length ; i++){
-            int num = 0;
-            for(char ch : targets[i].toCharArray()){
-                if(map.containsKey(ch)){
-                    num +=map.get(ch);
-                }else{
-                    num = -1;
+            for(int j = 0 ; j < targets[i].length() ; j++){
+                int check = Integer.MAX_VALUE;
+                for(String s : spell){
+                    int num = s.indexOf(targets[i].charAt(j));
+                    
+                    if(num == -1)continue;
+                    else if(num < check)check = num+1 ;                      
+                    
+                }
+                if(check == Integer.MAX_VALUE){
+                    answer[i] = -1;
                     break;
+                }else{
+                    answer[i] +=check;
                 }
             }
-            answer[i] +=num;
         }
         
         return answer;
