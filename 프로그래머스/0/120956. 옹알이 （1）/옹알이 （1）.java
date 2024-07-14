cf5fa7ade1c982e5726ebfb1c017d966d0d877e8
김범서 <beomkim7@naver.com>
2024-07-14 12:16:42 +0900

    [level 0] Title: 옹알이 (1), Time: 2.54 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,19 +1,17 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        
-        String [] check = {"aya", "ye", "woo", "ma"};
-        
-        for(String st : babbling){
-            for(String ch : check){
-                st = st.replace(ch," ");
-                st = st.trim();
-            }
-            if(st.equals("")){
-                answer++;
+        for(String check : babbling){
+            check = check.replace("aya"," ");
+            check = check.replace("ye"," ");
+            check = check.replace("woo"," ");
+            check = check.replace("ma"," ");
+            check = check.trim();
+            System.out.println(check);
+            if(check.isEmpty()){
+                answer += 1;                
             }
         }
-        
         return answer;
     }
 }
