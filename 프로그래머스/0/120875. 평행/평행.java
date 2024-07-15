13a272e86e64801e745068a529b1a62b859a4f64
김범서 <beomkim7@naver.com>
2024-07-16 08:56:45 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,23 +1,17 @@
 class Solution {
     
+    
+    public double bool(int [] a , int [] b){
+        return (double)(a[1]-b[1])/(a[0]-b[0]);
+    }
+    
+    public int solution(int[][] dots) {
+        int answer = 0;
+        if(bool(dots[0],dots[1])==bool(dots[2],dots[3]))answer = 1;
+        if(bool(dots[0],dots[2])==bool(dots[1],dots[3]))answer = 1;
+        if(bool(dots[0],dots[3])==bool(dots[1],dots[2]))answer = 1;
         
-        public double num(int[] a, int[] b){
-            return (double)(a[1]-b[1])/(a[0]-b[0]);
-        }
+        return answer;
         
-        
-        public boolean check(int[] a,int[] b,int[] c,int[] d){
-            return num(a,b) == num(c,d);
-            
-        }
-        public int solution(int[][] dots) {
-        if(check(dots[0],dots[1],dots[2],dots[3]))return 1;
-        if(check(dots[0],dots[2],dots[1],dots[3]))return 1;
-        if(check(dots[0],dots[3],dots[1],dots[2]))return 1;
-        if(check(dots[1],dots[2],dots[0],dots[3]))return 1;
-        
-        
-        
-        return 0;
     }
 }
