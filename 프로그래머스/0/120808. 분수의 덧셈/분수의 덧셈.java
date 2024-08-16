77af42cfae416dab14264c98a516e7080efc9a0f
김범서 <beomkim7@naver.com>
2024-08-16 12:36:31 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.23 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,21 +1,25 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] answer = new int [2];
         
-        int mom = numer1*denom2 + numer2*denom1;
-        int bro = denom1 * denom2;
-        int result = cal(mom,bro);
+        int mom = denom1 * denom2;
+        int bro = (numer2 * denom1) + (numer1 * denom2);
+        int result = check(bro,mom);
+        System.out.println(result);
         
-        int[] answer = {mom/result,bro/result};
+        answer[0] = bro/result;
+        answer[1] = mom/result;
         return answer;
     }
     
-    public int cal(int a , int b){
-        int temp = 0;
+    public int check(int a , int b){
+        int temp = 0 ;
         while(b != 0){
             temp = b;
             b = a % b;
-            a= temp;
+            a = temp;
         }
         return a;
     }
+    
 }
