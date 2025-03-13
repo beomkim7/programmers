16e7c0ca2bb145487d6a4f7feee86198037fde32
김범서 <beomkim7@naver.com>
2025-03-13 16:18:55 +0900

    [level 0] Title: 진료 순서 정하기, Time: 1.80 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cs
+++ C#/프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 public class Solution {
     public int[] solution(int[] emergency) {
-            int[] orders = emergency.OrderByDescending(x => x).ToArray();
-            int[] answer = emergency.Select(x => Array.IndexOf(orders, x) + 1).ToArray();
+            int[] answer = new int[emergency.Length];
 
+            for (int i = 0; i < emergency.Length; i++) answer[i] = emergency.Length - i;
+
+            Array.Sort(emergency, answer);
             return answer;
     }
 }
