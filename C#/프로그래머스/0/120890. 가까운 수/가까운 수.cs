a919f79775e24014bd1611ef78eca670ca4940e7
김범서 <beomkim7@naver.com>
2025-03-13 14:43:12 +0900

    [level 0] Title: 가까운 수, Time: 3.75 ms, Memory: 31.5 MB -BaekjoonHub

--- C#/프로그래머스/0/120890. 가까운 수/가까운 수.cs
+++ C#/프로그래머스/0/120890. 가까운 수/가까운 수.cs
@@ -3,16 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 public class Solution {
     public int solution(int[] array, int n) {
-            int answer = 0;
-            int[] check = (int[])array.Clone();
-            List<int> nums = new List<int>();
-
-            for(int i = 0; i <array.Length;i++) check[i] = Math.Abs(array[i] - n);
-
-            for (int i = 0; i < array.Length; i++)
-                if (check.Min() == check[i]) nums.Add(array[i]);
-
-            answer = nums.ToArray().Min();
+            int answer = array.OrderBy(x=>x).OrderBy(x=>Math.Abs(x-n)).ToArray().FirstOrDefault();
 
             return answer;
     }
