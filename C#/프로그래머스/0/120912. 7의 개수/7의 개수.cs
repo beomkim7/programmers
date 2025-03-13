bb2714119c40755a79b5a8cdeee1dd5551370d31
김범서 <beomkim7@naver.com>
2025-03-13 13:27:24 +0900

    [level 0] Title: 7의 개수, Time: 1.70 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/0/120912. 7의 개수/7의 개수.cs
+++ C#/프로그래머스/0/120912. 7의 개수/7의 개수.cs
@@ -1,14 +1,8 @@
 using System;
-
+using System.Linq;
 public class Solution {
     public int solution(int[] array) {
-                int answer = 0;
-                string chage = "";
-
-                for (int i = 0; i < array.Length; i++) chage += array[i].ToString();
-                for (int i = 0; i < chage.Length; i++) if (chage[i].ToString() == "7") answer++;
-                
-
-                return answer;
+int answer = string.Join("", array).Count(x => x == '7');
+            return answer;
     }
 }
