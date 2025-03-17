97ff344b04d02ab74377a2cb7643f025fa8f5f75
김범서 <beomkim7@naver.com>
2025-03-17 14:43:21 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 1.46 ms, Memory: 31.4 MB -BaekjoonHub

--- C#/프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cs
+++ C#/프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cs
@@ -2,16 +2,14 @@ using System;
 using System.Linq;
 public class Solution {
     public int solution(string my_string) {
-            foreach(char a in my_string)
-            {
-                if (!int.TryParse(a.ToString(), out int result)) my_string = my_string.Replace(a, ' ');
-            }
+            int answer = 0;
+            
 
-            int answer = my_string.Split(new string[] { " " },StringSplitOptions.RemoveEmptyEntries)
-                            .Sum(x=>int.Parse(x));
-            Console.WriteLine(my_string);
+            foreach(char c in my_string.ToCharArray())
+                if (!int.TryParse(c.ToString(), out int result)) my_string=my_string.Replace(c, ' ');
 
-
+            answer = my_string.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Sum(x => int.Parse(x));
+            
             return answer;
     }
 }
