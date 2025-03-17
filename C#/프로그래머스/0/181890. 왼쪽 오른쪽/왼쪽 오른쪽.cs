da3ebf4dfd956f486b530980b1d0782b832b9486
김범서 <beomkim7@naver.com>
2025-03-17 17:09:42 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.65 ms, Memory: 31 MB -BaekjoonHub

--- C#/프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.cs
+++ C#/프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.cs
@@ -1,30 +1,26 @@
 using System;
 using System.Collections.Generic;
+public class Solution {
+    public string[] solution(string[] str_list) {
+            List<string> answer = new List<string>();
 
-public class Solution
-{
-    public string[] solution(string[] str_list)
-    {
-        List<string> answer = new List<string>();
-
-        for (int i = 0; i < str_list.Length; i++)
-        {
-            if (str_list[i] == "l")
+            for(int i = 0; i < str_list.Length;i++)
             {
-                // "l"의 왼쪽 요소들만 저장
-                for (int j = 0; j < i; j++)
-                    answer.Add(str_list[j]);
-                return answer.ToArray();
-            }
-            else if (str_list[i] == "r")
-            {
-                // "r"의 오른쪽 요소들만 저장
-                for (int j = i + 1; j < str_list.Length; j++)
-                    answer.Add(str_list[j]);
-                return answer.ToArray();
-            }
-        }
+                if (str_list[i].Equals("l"))
+                {
+                    for (int j = 0; j < i; j++)
+                        answer.Add(str_list[j]);
+                    return answer.ToArray();
+                }
 
-        return new string[0]; // "l"이나 "r"이 없을 경우 빈 배열 반환
+                else if (str_list[i].Equals("r"))
+                {
+                    for (int j = i+1; j < str_list.Length; j++)
+                        answer.Add(str_list[j]);
+                    return answer.ToArray();
+                }
+
+            }
+            return new string[0];
     }
 }
