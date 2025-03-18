960b0caf0ab2d92e38e074704732b11f56e33462
김범서 <beomkim7@naver.com>
2025-03-18 10:07:20 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 68.23 ms, Memory: 34.6 MB -BaekjoonHub

--- C#/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.cs
+++ C#/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.cs
@@ -2,14 +2,23 @@ using System;
 
 public class Solution {
     public string solution(string my_string, int[,] queries) {
-        
-        char[] arr = my_string.ToCharArray();
-        for(int i = 0 ; i <queries.GetLength(0);i++){
-            int start = queries[i,0];
-            int end = queries[i,1];
-            Array.Reverse(arr,start,end-start+1);
-        }
-        
-        return new string(arr);
+            string answer = "";
+
+            for(int i = 0; i < queries.GetLength(0); i++)
+            {
+                int start = queries[i, 0];
+                int end = queries[i, 1];
+                string arr = "";
+                string reverseArr = "";
+
+                for (int j = start; j <= end; j++) arr += my_string[j];
+                for (int k = arr.Length - 1; k >= 0; k--) reverseArr += arr[k];
+
+                my_string = my_string.Remove(start, end-start+1);
+                my_string = my_string.Insert(start, reverseArr) ;
+
+            }
+
+            return my_string;
     }
 }
