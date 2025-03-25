a3e09d8a491e7d0445cda6623ef226cb4d1c7b62
김범서 <beomkim7@naver.com>
2025-03-25 16:02:26 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 9.18 ms, Memory: 31.7 MB -BaekjoonHub

--- C#/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cs
+++ C#/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cs
@@ -1,21 +1,16 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-
+using System.Collections.Generic;
 public class Solution {
     public int solution(int[] rank, bool[] attendance) {
             int answer = 0;
-
             Dictionary<int, int> dict = new Dictionary<int, int>();
+            for(int i = 0; i < rank.Length; i++)
+                if (attendance[i]) dict.Add(rank[i], i);
 
-            int[] rankIndex = Enumerable.Range(0, rank.Length).ToArray();
-            for(int i = 0; i < rank.Length;i++)
-                if (attendance[i])                
-                    dict.Add(rank[i], rankIndex[i]);
+            var sorted = dict.OrderBy(x => x.Key).Select(x=>x.Value).ToArray();
 
-            var sorted = dict.OrderBy(x => x.Key).Select(x => x.Value).ToArray();
-
-                answer += sorted[0]*10000 + sorted[1] * 100 +sorted[2];
+            answer = sorted[0] * 10000 + sorted[1] * 100 + sorted[2];          
 
             return answer;
     }
