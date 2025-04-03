4c634f25a645521b5f105224590f6d485aebd4ad
김범서 <beomkim7@naver.com>
2025-04-03 11:15:18 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.82 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cs
+++ C#/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cs
@@ -1,8 +1,12 @@
 using System;
-using System.Linq;
+
 public class Solution {
     public string solution(string my_string, int s, int e) {
-            string answer = my_string.Remove(s,e-s+1).Insert(s,new string(my_string.Substring(s,e-s+1).Reverse().ToArray()));
+            string answer = "";
+            char[] kCheck = my_string.ToCharArray();
+
+            Array.Reverse(kCheck, s, e - s + 1);
+            answer = new string(kCheck);
             return answer;
     }
 }
