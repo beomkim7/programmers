8e32846d10fbde37a1f7c65e9ca52d7f09457f1f
김범서 <beomkim7@naver.com>
2025-04-03 13:56:05 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.54 ms, Memory: 31 MB -BaekjoonHub

--- C#/프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.cs
+++ C#/프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.cs
@@ -2,15 +2,12 @@ using System;
 
 public class Solution {
     public int[,] solution(int[] num_list, int n) {
-        int[,] answer = new int[num_list.Length/n,n];
-        int index = 0;
-        
-        for(int i = 0 ; i < num_list.Length;i +=n)
-            for(int j = 0 ; j < n ; j++){
-                answer[i/n,j]=num_list[index++];
-            }
-                
-                
-        return answer;
+            int[,] answer = new int[num_list.Length / n, n];
+
+            for (int i = 0; i < answer.GetLength(0); i++)
+                for (int j = 0; j < answer.GetLength(1); j++)
+                    answer[i, j] = num_list[n*i + j];
+
+            return answer;
     }
 }
