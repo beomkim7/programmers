e4ff7e3df4a61660579b63ac3a705d92952938d5
김범서 <beomkim7@naver.com>
2025-04-04 16:23:41 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.85 ms, Memory: 31 MB -BaekjoonHub

--- C#/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cs
+++ C#/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cs
@@ -2,7 +2,7 @@ using System;
 using System.Linq;
 public class Solution {
     public int[] solution(int[] numbers) {
-int[] answer = numbers.Select(x => x * 2).ToArray();
-            return answer;
+        int[] answer = numbers.Select(x=>x*2).ToArray();
+        return answer;
     }
 }
