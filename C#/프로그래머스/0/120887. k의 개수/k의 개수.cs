ec35038d85c18bc27793c2605020d71be9ffb9ee
김범서 <beomkim7@naver.com>
2025-04-07 11:34:32 +0900

    [level 0] Title: k의 개수, Time: 37.99 ms, Memory: 34.9 MB -BaekjoonHub

--- C#/프로그래머스/0/120887. k의 개수/k의 개수.cs
+++ C#/프로그래머스/0/120887. k의 개수/k의 개수.cs
@@ -1,10 +1,11 @@
-using System;
-using System.Linq;
+using System;using System.Linq;
+
 public class Solution {
     public int solution(int i, int j, int k) {
             int answer = 0;
             for (int check = i; check <= j; check++)
-                if (check.ToString().Contains(k.ToString())) answer += check.ToString().Count(x => x.ToString() == k.ToString());
+                if (check.ToString().Contains(k.ToString()))
+                    answer += check.ToString().Count(x => x.ToString() == k.ToString());
             return answer;
     }
 }
