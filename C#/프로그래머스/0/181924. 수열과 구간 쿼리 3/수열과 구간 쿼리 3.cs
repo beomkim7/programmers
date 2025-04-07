0edada1c94c8b678653ae735606b1c4e88f7e8c4
김범서 <beomkim7@naver.com>
2025-04-07 10:53:35 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.43 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cs
+++ C#/프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cs
@@ -2,16 +2,19 @@ using System;
 
 public class Solution {
     public int[] solution(int[] arr, int[,] queries) {
-            for (int i = 0; i < queries.GetLength(0);i++)
+            int[] answer = (int[]) arr.Clone();
+
+            for (int i = 0; i < queries.GetLength(0); i++) 
             {
                 int st = queries[i, 0];
                 int ed = queries[i, 1];
-                int check = arr[st];
 
-                arr[st] = arr[ed];
-                arr[ed] = check;
+                int temp = answer[st];
+
+                answer[st] = answer[ed];
+                answer[ed] = temp;
 
             }
-                return arr;
+            return answer;
     }
 }
