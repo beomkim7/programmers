2cd3c054fb2556641128e8022f35ba8b983d344c
김범서 <beomkim7@naver.com>
2025-04-07 13:37:51 +0900

    [level 0] Title: 문자열 묶기, Time: 14.28 ms, Memory: 51.9 MB -BaekjoonHub

--- C#/프로그래머스/0/181855. 문자열 묶기/문자열 묶기.cs
+++ C#/프로그래머스/0/181855. 문자열 묶기/문자열 묶기.cs
@@ -1,19 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;using System.Linq;
 
 public class Solution {
     public int solution(string[] strArr) {
-            int answer = 0;
-            Dictionary<int, int> dict = new Dictionary<int, int>();
-            for(int i = 0; i < strArr.Length; i++)
-            {
-                if (!dict.ContainsKey(strArr[i].Length)) dict.Add(strArr[i].Length, 1);
-                else dict[strArr[i].Length] += 1;
-            }
-
-            answer = dict.Values.Max();
-
+            int answer = strArr.GroupBy(x=>x.Length)
+                                .OrderByDescending(x=>x.Count())
+                                .First().Count();
             return answer;
     }
 }
