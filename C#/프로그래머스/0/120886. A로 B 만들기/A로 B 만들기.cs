d73ae84e206f517aa6ac10202d515e9b5cf971fd
김범서 <beomkim7@naver.com>
2025-04-07 11:37:11 +0900

    [level 0] Title: A로 B 만들기, Time: 4.24 ms, Memory: 31.5 MB -BaekjoonHub

--- C#/프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.cs
+++ C#/프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.cs
@@ -2,8 +2,8 @@ using System;
 using System.Linq;
 public class Solution {
     public int solution(string before, string after) {
-int answer = 
-            string.Concat(before.OrderBy(x => x)) == string.Concat(after.OrderBy(x => x)) ? 1 : 0;
+int answer = string.Concat(before.OrderBy(x=>x))==string.Concat(after.OrderBy(x=>x))? 1:0;
+            
             return answer;
     }
 }
