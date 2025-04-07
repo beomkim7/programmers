382ca6d95849c15c55af59a0249e33c8f2c1a9c4
김범서 <beomkim7@naver.com>
2025-04-07 13:50:47 +0900

    [level 0] Title: 리스트 자르기, Time: 1.08 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cs
+++ C#/프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cs
@@ -1,29 +1,29 @@
-using System;
-using System.Collections.Generic;
+using System;using System.Collections.Generic;
+
 public class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-                       List<int> lst = new List<int>();
+            List<int> answer = new List<int>();
+
             switch (n)
             {
                 case 1:
                     for (int i = 0; i <= slicer[1]; i++)
-                        lst.Add(num_list[i]);
+                        answer.Add(num_list[i]);
                     break;
                 case 2:
                     for (int i = slicer[0]; i < num_list.Length; i++)
-                        lst.Add(num_list[i]);
+                        answer.Add(num_list[i]);
                     break;
                 case 3:
                     for (int i = slicer[0]; i <= slicer[1]; i++)
-                        lst.Add(num_list[i]);
-                    break;
+                        answer.Add(num_list[i]);
+                        break;
                 case 4:
-                    for (int i = slicer[0]; i <= slicer[1]; i=i+slicer[2])
-                        lst.Add(num_list[i]);
+                    for (int i = slicer[0]; i <= slicer[1]; i +=slicer[2])
+                        answer.Add(num_list[i]);
                     break;
-
             }
 
-            return lst.ToArray();
+            return answer.ToArray();
     }
 }
