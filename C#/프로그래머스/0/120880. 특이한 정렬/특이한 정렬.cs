159f9c4fd4f057a2109be172a75a369f878ac36b
김범서 <beomkim7@naver.com>
2025-04-10 17:27:10 +0900

    [level 0] Title: 특이한 정렬, Time: 5.58 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cs
@@ -0,0 +1,8 @@
+using System;using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] numlist, int n) {
+int[] answer = numlist.OrderBy(x=>Math.Abs(x-n)).ThenByDescending(x=>x).ToArray();
+            return answer;
+    }
+}
