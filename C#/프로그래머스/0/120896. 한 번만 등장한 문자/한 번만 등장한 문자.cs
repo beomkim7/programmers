ec1b2338935ba2e23f9e415636cf0cccade49fea
김범서 <beomkim7@naver.com>
2025-04-25 16:48:28 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 12.59 ms, Memory: 31 MB -BaekjoonHub

--- C#/프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.cs
+++ C#/프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.cs
@@ -1,22 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 public class Solution {
     public string solution(string s) {
-            string answer = "";
-            Dictionary<char, int> dict = new Dictionary<char, int>();
-
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (dict.ContainsKey(s[i])) dict[s[i]]++;
-                else dict.Add(s[i], 1);
-            }
-
-            foreach (char c in dict.Keys)
-                if (dict[c] == 1) answer += c;
-
-            answer = new string(answer.OrderBy(x => x).ToArray());
-
+            string answer = new string (s.GroupBy(x => x, y => y, (x, y) => new { key = x, value = y.Count() })
+                                .Where(x => x.value == 1)
+                                .Select(x => x.key)
+                                .OrderBy(x => x)
+                                .ToArray());
             return answer;
     }
 }
