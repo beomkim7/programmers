093e8ddbe0338c09b797a4eebb756a5cc0cdb5a4
김범서 <beomkim7@naver.com>
2025-04-28 16:49:54 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.32 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
+++ C#/프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
@@ -1,14 +1,15 @@
 using System;
 
 public class Solution {
-    public int solution(int balls, int share) {
-            double answer = 1;
-
-            for (int i = 1; i <= balls; i++) answer *= i;
-            for (int i = 1; i <= share; i++) answer /= i;
-            for (int i = 1; i <= balls - share; i++) answer /= i;
-
-            
-return Convert.ToInt32(answer);
-    }
+        public double solution(int balls, int share)
+        {
+            int answer = 0;
+            double up = 1;
+            double down = 1;
+            for (int i = share + 1; i <= balls; i++) up *= i;
+            for (int i = 1; i <= balls-share; i++) down *= i;
+            answer = (int)(up / down);
+            if (balls == share) answer = 1;
+            return answer;
+        }
 }
