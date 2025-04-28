b9b106761ebd4da6b3a785d3357cbcbb01673e40
김범서 <beomkim7@naver.com>
2025-04-28 13:45:03 +0900

    [level 0] Title: qr code, Time: 0.86 ms, Memory: 31.8 MB -BaekjoonHub

--- C#/프로그래머스/0/181903. qr code/qr code.cs
+++ C#/프로그래머스/0/181903. qr code/qr code.cs
@@ -1,11 +1,14 @@
 using System;
-using System.Linq;
+
 public class Solution {
     public string solution(int q, int r, string code) {
             string answer = "";
 
-            answer = new string(code.Where((x, index) => index % q == r).ToArray());
+            for (int i = r; i < code.Length; i += q)
+                answer += code[i].ToString();
+                
             
+
             return answer;
     }
 }
