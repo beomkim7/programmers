0d70cbcc7c39f67060d01b91c4402d7d9026b23c
김범서 <beomkim7@naver.com>
2025-04-28 17:00:49 +0900

    [level 0] Title: 그림 확대, Time: 0.56 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/0/181836. 그림 확대/그림 확대.cs
+++ C#/프로그래머스/0/181836. 그림 확대/그림 확대.cs
@@ -1,22 +1,16 @@
-using System;using System.Collections.Generic;using System.Text;
-
+using System;
+using System.Text;
 public class Solution {
     public string[] solution(string[] picture, int k) {
-            List<string> answer = new List<string>();
+            string[] answer = new string[picture.Length*k];
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < picture.Length; i++)
+            for(int i = 0; i < picture.Length; i++)
             {
-                for (int j = 0; j < picture[i].Length; j++)
-                    for (int z = 0; z < k; z++)
-                        sb.Append(picture[i][j]);
-
-                for(int q = 0; q <k;q++)
-                    answer.Add(sb.ToString());
-
+                for (int j = 0; j < picture[i].Length; j++) sb.Append(picture[i][j], k);
+                for (int z = 0; z < k; z++) answer[i * k + z] = sb.ToString();
                 sb.Clear();
             }
 
-
-            return answer.ToArray();
+            return answer;
     }
 }
