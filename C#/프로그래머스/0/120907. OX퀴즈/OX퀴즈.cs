d874998f938986583ac8ae6e899a07174d514339
김범서 <beomkim7@naver.com>
2025-04-29 14:42:05 +0900

    [level 0] Title: OX퀴즈, Time: 0.83 ms, Memory: 31.4 MB -BaekjoonHub

--- C#/프로그래머스/0/120907. OX퀴즈/OX퀴즈.cs
+++ C#/프로그래머스/0/120907. OX퀴즈/OX퀴즈.cs
@@ -1,19 +1,26 @@
 using System;
 
 public class Solution {
-    public string[] solution(string[] quiz) {
+        public string[] solution(string[] quiz)
+        {
             string[] answer = new string[quiz.Length];
 
-            for(int j = 0; j<quiz.Length;j++ )
+            for(int i = 0; i < quiz.Length; i++)
             {
-                string input = "";
-                string[] check = quiz[j].Split(new string[] { " " }, StringSplitOptions.None);
-                
-                if(check[1]=="-") input = int.Parse(check[0])- int.Parse(check[2])==int.Parse(check[4])?"O":"X";
-                else input = int.Parse(check[0]) + int.Parse(check[2]) == int.Parse(check[4]) ? "O" : "X";
+                string[] stArr = quiz[i].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
 
-                answer[j] = input;
+                answer[i] =Calc(stArr[0], stArr[2], stArr[1]) ==int.Parse(stArr[4])?"O":"X"; //"X", "O"
             }
+
             return answer;
-    }
+        }
+
+        public int Calc(string num1,string num2,string check)
+        {
+            int result = 0;
+            if(check == "+") result = int.Parse(num1) + int.Parse(num2);
+            else if(check == "-") result = int.Parse(num1) - int.Parse(num2);
+
+            return result;
+        }
 }
