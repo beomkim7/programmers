33debc6063550e5efc6071dacc21c827c2949337
김범서 <beomkim7@naver.com>
2025-04-29 10:18:49 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 1.57 ms, Memory: 31.4 MB -BaekjoonHub

--- C#/프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.cs
+++ C#/프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.cs
@@ -1,15 +1,23 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 public class Solution {
     public int solution(int[,] dots) {
             int answer = 0;
-            int minX = Enumerable.Range(0, dots.GetLength(0)).Select(x => dots[x, 0]).Min();
-            int maxX = Enumerable.Range(0, dots.GetLength(0)).Select(x => dots[x, 0]).Max();
-            int minY = Enumerable.Range(0, dots.GetLength(0)).Select(x => dots[x, 1]).Min();
-            int maxY = Enumerable.Range(0, dots.GetLength(0)).Select(x => dots[x, 1]).Max();
-            answer = (maxX - minX) * (maxY - minY);
 
+            List<int> XRange = new List<int>();
+            List<int> YRange = new List<int>();
 
+            for(int i = 0; i < dots.GetLength(0); i++)
+            {
+                XRange.Add(dots[i, 0]);
+                YRange.Add(dots[i, 1]);
+            }
+
+            int x = XRange.Max() - XRange.Min();
+            int y = YRange.Max() - YRange.Min();
+
+            answer = x * y;
             return answer;
     }
 }
