578fc314b19e5e63b2c9e765d1f460889f8ddb76
김범서 <beomkim7@naver.com>
2025-04-29 10:38:15 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.21 ms, Memory: 31.5 MB -BaekjoonHub

--- C#/프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cs
+++ C#/프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cs
@@ -2,15 +2,21 @@ using System;
 
 public class Solution {
     public int solution(int chicken) {
-            int answer = 0;            
-            
-            while (chicken >= 10)
-            {
-                chicken -= 10;
-                chicken++;
-                answer++;
-            }
+            int answer = 0;
+            int coupon = 0;
 
+            while(chicken > 0)
+            {                
+                answer += chicken / 10;                                         //108 , 118 , 119 ,119
+                coupon += chicken % 10;                                         //1 ,   9   , 9 , 10
+                if(coupon >= 10)     
+                {
+                    answer += coupon / 10;                                      //120
+                    coupon = chicken % 10;                                      
+                }
+                chicken /= 10;                                                  //108   ,10   ,1 ,0                                    
+
+            }
             return answer;
     }
 }
