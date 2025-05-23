cef0cb569c20b53548aeb8a4bb9d1c1398882843
김범서 <beomkim7@naver.com>
2025-05-23 15:37:45 +0900

    [level 0] Title: 다항식 더하기, Time: 4.93 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/0/120863. 다항식 더하기/다항식 더하기.cs
+++ C#/프로그래머스/0/120863. 다항식 더하기/다항식 더하기.cs
@@ -1,33 +1,29 @@
 using System;
 using System.Linq;
 public class Solution {
-        public string solution(string polynomial)
-        {
+    public string solution(string polynomial) {
             string answer = "";
 
-            string[] stArr = polynomial.Split(new string[] { " + " }, StringSplitOptions.RemoveEmptyEntries);
-            
-            string[] xArr = stArr.Where(x => x.Contains("x")).ToArray();
-            string[] numArr = stArr.Where(x => !x.Contains("x")).ToArray();
+            string[] arr = polynomial.Split(new string[] { " + " }, StringSplitOptions.RemoveEmptyEntries);
 
-            int xValue = 0;
-            for (int i = 0; i < xArr.Length; i++)
-                xValue += xArr[i] == "x" ? 1 : Change(xArr[i]);
+            string[] arrX = arr.Where(x => x.Contains("x")).ToArray();
+            string[] arrNum = arr.Where(x => !x.Contains("x")).ToArray();
 
-            int numValue = numArr.Select(x => int.Parse(x)).Sum();
+            string StrX = "";
+            string StrNum = "";
 
-            string st_X = xValue == 0 ? "" : xValue == 1 ? "x" : xValue + "x";
-            string st_num = numValue == 0 ? "" : numValue.ToString();
+            int xnum = 0;
+            for (int i = 0; i < arrX.Length; i++)
+                xnum += arrX[i].Replace("x", "") == "" ? 1 : int.Parse(arrX[i].Replace("x", ""));
 
-            if (!string.IsNullOrEmpty(st_X) && !string.IsNullOrEmpty(st_num)) answer = st_X + " + " + st_num;
-            else answer = st_X +  st_num;
+            StrX = xnum == 0 ? "" : xnum == 1? "x": xnum.ToString() +"x";
+
+            int num = arrNum.Select(x => int.Parse(x)).Sum();
+            StrNum = num == 0 ? "" : num.ToString();
+
+            if (!string.IsNullOrEmpty(StrX) && !string.IsNullOrEmpty(StrNum)) answer = StrX + " + " + StrNum;
+            else answer = StrX + StrNum;
 
             return answer;
-        }
-
-        public int Change(string X)
-        {
-            Console.WriteLine(X);
-            return int.Parse(X.Remove(X.IndexOf("x"), 1));
-        }
+    }
 }
