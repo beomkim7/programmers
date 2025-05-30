f8e16d55df50f195e13af5f06ef629e68822602c
김범서 <beomkim7@naver.com>
2025-05-30 14:05:49 +0900

    [level 0] Title: 배열 조각하기, Time: 1.58 ms, Memory: 32.8 MB -BaekjoonHub

--- C#/프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cs
+++ C#/프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cs
@@ -1,22 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-public class Solution {
-    public int[] solution(int[] arr, int[] query) {
-            List<int> answer = arr.ToList();
+    public class Solution
+    {
+        public int[] solution(int[] arr, int[] query)
+        {
+            List<int> check = arr.ToList();
 
             for(int i = 0; i < query.Length; i++)
             {
-                if(i % 2 == 0)
-                {
-                    answer.RemoveRange(query[i]+1, answer.Count - query[i]-1);
-                }
+                if (i % 2 == 0)
+                    check.RemoveRange(query[i]+1, check.Count - query[i]-1);
                 else
-                {
-                    answer.RemoveRange(0,query[i]);
-                }
+                    check.RemoveRange(0, query[i]);
             }
 
-            return answer.ToArray();
+            return check.ToArray();
+        }
     }
-}
