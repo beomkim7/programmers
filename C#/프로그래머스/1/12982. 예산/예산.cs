1bd33c0b63c5221014d5ecc4a1c30720c34e6528
김범서 <beomkim7@naver.com>
2025-06-27 08:55:09 +0900

    [level 1] Title: 예산, Time: 5.88 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ C#/프로그래머스/1/12982. 예산/예산.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+public class Solution {
+    public int solution(int[] d, int budget) {
+            int answer = 0;
+            if (d.Sum() == budget) answer = d.Length;
+            else
+            {                
+                int check = 0;
+                d = d.OrderBy(x=>x).ToArray();
+                for(int i = 0; i < d.Length; i++)
+                {
+                    check += d[i];
+                    if (check > budget) break;
+                    answer++;
+                }
+            }
+            return answer;
+    }
+}
