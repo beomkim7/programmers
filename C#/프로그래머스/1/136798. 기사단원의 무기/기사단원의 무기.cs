072069a35dd33136ee4f4567ac4c8b641f1d367f
김범서 <beomkim7@naver.com>
2025-06-30 17:29:04 +0900

    [level 1] Title: 기사단원의 무기, Time: 150.02 ms, Memory: 35.5 MB -BaekjoonHub

--- C#/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
+++ C#/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
@@ -1,22 +1,26 @@
 using System;
-using System.Linq;using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
 public class Solution {
     public int solution(int number, int limit, int power) {
             int answer = 0;
-
-            List<int> nArr = new List<int>();
-            int check = 1;
-            while (check <= number)
+            List<int> check = new List<int>();
+            for(int i = 1; i <=number; i++)
             {
-                int num = 1;
-                for (int i = 1; i <= check / 2; i++)
-                    if (check % i == 0) num++;
-                nArr.Add(num);                
-                check++;
+                int num = 0;
+                for(int j = 1; j*j <= i; j++)
+                {
+                    if(i%j == 0)
+                    {
+                        num++;
+                        if (j != i / j) num++;
+                    }
+                }
+                check.Add(num);
             }
-            Console.WriteLine(string.Join(",", nArr));
-            answer += nArr.Where(x => x <= limit).Sum();
-            answer += nArr.Where(x => x > limit).Count() * power;
+Console.WriteLine(string.Join(",", check));
+            answer = check.Where(x => x <= limit).Sum();
+            answer += check.Where(x => x > limit).Count() * power;
 
             return answer;
     }
