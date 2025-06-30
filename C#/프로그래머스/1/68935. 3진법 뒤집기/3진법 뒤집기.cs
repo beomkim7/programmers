f9c5e86fbefd8b42c08d3ef9c169135c9e81a6f0
김범서 <beomkim7@naver.com>
2025-06-30 10:38:25 +0900

    [level 1] Title: 3진법 뒤집기, Time: 1.63 ms, Memory: 31.8 MB -BaekjoonHub

--- C#/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cs
+++ C#/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cs
@@ -3,17 +3,16 @@ using System;
 public class Solution {
     public int solution(int n) {
             int answer = 0;
-            string num = "";
-            while (true)
-            {                
-                num += (n % 3).ToString();
+            string Snum = "";
+            while (n > 0)
+            {
+                Snum += (n % 3).ToString();
                 n = n / 3;
-                if (n == 0) break;
             }
 
-            for (int i = 0; i < num.Length; i++)
-                answer += (int)(Math.Pow(3, num.Length-1-i))* int.Parse(num[i].ToString());
-
+            for (int i = 0; i < Snum.Length; i++)
+                answer += (int)(Math.Pow(3, Snum.Length - i - 1)) * int.Parse(Snum[i].ToString());
+            
             return answer;
     }
 }
