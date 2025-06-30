9daf42529150c550b97f4938459ea12bc3d01073
김범서 <beomkim7@naver.com>
2025-06-30 15:02:10 +0900

    [level 1] Title: 콜라 문제, Time: 0.27 ms, Memory: 31.4 MB -BaekjoonHub

--- C#/프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
+++ C#/프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -3,11 +3,12 @@ using System;
 public class Solution {
     public int solution(int a, int b, int n) {
             int answer = 0;
-            while (n >= a)
+
+            while (n > 0)
             {
-        int exchanged = (n / a) * b;
-        answer += exchanged;
-        n = (n % a) + exchanged;
+                answer += n / a * b;
+                n = n % a + n / a *b;
+if (n < a) break;
             }
 
             return answer;
