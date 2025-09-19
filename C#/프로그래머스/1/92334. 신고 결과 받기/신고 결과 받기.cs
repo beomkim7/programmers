ad4105ee8f15858ba4392deace0588da5e3bedc3
김범서 <beomkim7@naver.com>
2025-09-19 09:52:36 +0900

    [level 1] Title: 신고 결과 받기, Time: 2949.08 ms, Memory: 77.5 MB -BaekjoonHub

--- C#/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
+++ C#/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
@@ -1,32 +1,32 @@
 using System;
 using System.Linq;
-
 public class Solution {
     public int[] solution(string[] id_list, string[] report, int k) {
-int length = id_list.Length;
+            int length = id_list.Length;
             int[] answer = new int[length];
-            int[] receiver = new int[length];
 
-            report = report.Distinct().ToArray();
-            foreach(string s in report)
-            {
-                string reported = s.Split(' ')[1];
-                int reported_idx = Array.IndexOf(id_list, reported);
-                receiver[reported_idx]++;
-            }
+            int[] check = new int[length];
+            report = report.AsEnumerable().Distinct().ToArray();
 
             for(int i = 0; i < report.Length; i++)
             {
-                string reported = report[i].Split(' ')[1];
-                int reported_idx = Array.IndexOf(id_list, reported);
-
-                if (receiver[reported_idx] >= k)
-                {
-                    string reporter = report[i].Split(' ')[0];
-                    int reporter_idx = Array.IndexOf(id_list, reporter);
-                    answer[reporter_idx]++;
-                }
+                string s = report[i].Split(' ')[1];
+                int num= Array.IndexOf(id_list, s);
+                check[num]++;
             }
+
+            int[] lcheck = new int[length];
+            for (int i = 0; i < report.Length; i++)
+            {
+                string p = report[i].Split(' ')[0];
+                string s = report[i].Split(' ')[1];
+
+                int nump = Array.IndexOf(id_list, p);
+                int num = Array.IndexOf(id_list, s);
+
+                if (check[num] >= k) answer[nump]++;
+            }
+
 
             return answer;
     }
