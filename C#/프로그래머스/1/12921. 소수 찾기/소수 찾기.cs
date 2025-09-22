fe4428c3692e0cf5555541720eeecd3463a2ed13
김범서 <beomkim7@naver.com>
2025-09-22 17:04:59 +0900

    [level 1] Title: 소수 찾기, Time: 6.09 ms, Memory: 31.8 MB -BaekjoonHub

--- C#/프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
+++ C#/프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
@@ -1,21 +1,18 @@
 public class Solution {
     public int solution(int n) {
-    bool[] isNotPrime = new bool[n + 1]; // false면 소수
-    int answer = 0;
+int answer = 0;
+            bool[] check = new bool[n + 1];
 
-    for (int i = 2; i <= n; i++)
-    {
-        if (!isNotPrime[i])
-        {
-            answer++;
-
-            for (int j = i * 2; j <= n; j += i)
+            for(int i = 2; i <= n; i++)
             {
-                isNotPrime[j] = true;
+                if (!check[i])
+                {
+                    answer++;
+                    for (int j = i*2; j <= n; j += i)
+                        check[j] = true;
+                }
             }
-        }
-    }
 
-    return answer;
+            return answer;
     }
 }
