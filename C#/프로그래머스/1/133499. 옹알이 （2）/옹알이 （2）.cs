d91e7c698e86760ab4f3b12cbb4daf0493627a45
김범서 <beomkim7@naver.com>
2025-09-22 16:42:37 +0900

    [level 1] Title: 옹알이 (2), Time: 4.60 ms, Memory: 31.8 MB -BaekjoonHub

--- C#/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cs
+++ C#/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cs
@@ -2,47 +2,44 @@ using System;
 
 public class Solution {
     public int solution(string[] babbling) {
-  int answer = 0;
-    string[] canSay = { "aya", "ye", "woo", "ma" };
+int answer = 0;
+            string[] stArr = new string[] { "aya", "ye", "woo", "ma" };
 
-    foreach (string b in babbling)
-    {
-        string temp = b;
-        string prev = "";
-        bool isValid = true;
-
-        while (temp.Length > 0)
-        {
-            bool matched = false;
-
-            foreach (string word in canSay)
+            for(int i = 0; i < babbling.Length; i++)
             {
-                if (temp.StartsWith(word))
-                {
-                    if (prev == word) // 연속된 발음 금지
+                string temp = babbling[i];
+                bool isVal = true;
+                string prev = "";
+
+                while (temp.Length > 0)
+                {                    
+                    bool matched = false;
+
+                    foreach(string st in stArr)
                     {
-                        isValid = false;
+                        if (temp.StartsWith(st))
+                        {
+                            if(prev == st)
+                            {
+                                isVal = false;
+                                break;
+                            }
+
+                            temp = temp.Substring(st.Length);
+                            prev = st;
+                            matched = true;
+                            break;
+                        }
+                    }
+                    if (!isVal || !matched)
+                    {
+                        isVal = false;
                         break;
                     }
-
-                    temp = temp.Substring(word.Length);
-                    prev = word;
-                    matched = true;
-                    break;
                 }
+                if (isVal) answer++;
             }
 
-            if (!matched || !isValid)
-            {
-                isValid = false;
-                break;
-            }
-        }
-
-        if (isValid)
-            answer++;
-    }
-
-    return answer;
+            return answer;
     }
 }
