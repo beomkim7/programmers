b05f5c75ff4499228357e35ff86fa03d7380bea8
김범서 <beomkim7@naver.com>
2025-09-22 14:45:14 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 7.29 ms, Memory: 31.5 MB -BaekjoonHub

--- C#/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ C#/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
 
 ### 성능 요약
 
-메모리: 31.5 MB, 시간: 0.90 ms
+메모리: 31.5 MB, 시간: 7.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 18일 16:15:42
+2025년 09월 22일 14:45:13
 
 ### 문제 설명
 
--- C#/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
+++ C#/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
@@ -3,18 +3,18 @@ using System.Linq;
 public class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
             int[] answer = new int[2];
-            int count = 7;
-            int bonus = 0;
-            foreach(int i in lottos)
-            {
-                if (i == 0) bonus--;
-                else
-                {
-                    if (win_nums.Contains(i)) count--;
-                }
-            }
-            answer[0] = (count + bonus) == 7?6: (count + bonus);
-            answer[1] = count == 7?6:count;
+
+            int rank = 7;
+            int count = lottos.AsEnumerable().Where(x => x == 0).Count();
+
+            lottos = lottos.AsEnumerable().OrderByDescending(x => x).ToArray();
+            win_nums = win_nums.AsEnumerable().OrderByDescending(x => x).ToArray();
+
+            for (int i = 0; i < lottos.Length; i++)
+                if (win_nums.Contains(lottos[i])) rank--;
+
+            answer[0] = rank - count==7?6:rank-count;
+            answer[1] = rank==7?6:rank;
 
             return answer;
     }
