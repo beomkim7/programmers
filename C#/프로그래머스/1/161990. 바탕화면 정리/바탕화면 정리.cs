642dfa86d4e9eb0f8b78d31a1c7076540558345c
김범서 <beomkim7@naver.com>
2025-09-25 14:31:35 +0900

    [level 1] Title: 바탕화면 정리, Time: 5.21 ms, Memory: 31.7 MB -BaekjoonHub

--- C#/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
+++ C#/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
@@ -2,54 +2,39 @@ using System;
 
 public class Solution {
     public int[] solution(string[] wallpaper) {
-            int[] answer = new int[4];
+int[] answer = new int[4];
+
             int answer1 = 0;
             int answer2 = int.MaxValue;
             int answer3 = 0;
             int answer4 = 0;
-            bool Fcheck = true;
 
-            while (Fcheck)
+            foreach(string s in wallpaper)
             {
-                foreach(string s in wallpaper)
+                if (s.Contains("#")) break;
+                answer1++;
+            }
+
+            foreach(string s in wallpaper)
+            {
+                if (s.Contains("#"))
                 {
-                    if (s.IndexOf("#") == -1) answer1++;
-                    else 
-                    {
-                        Fcheck = false;
-                        break;
-                    }
-                    
+                    int check = s.IndexOf('#');
+                    if (answer2 > check) answer2 = check;
+                    check = s.LastIndexOf("#");
+                    if (answer4 <= check) answer4 = check + 1;
                 }
             }
 
-            
-                int check = 0;
-                foreach (string s in wallpaper)
-                {                    
-                    if (s.Contains("#"))
-                    {
-                        check = s.IndexOf('#');
-                        if (answer2 > check) answer2 = check;
-                        check = s.LastIndexOf('#');
-                        if (answer4 <= check) answer4 = check + 1;
-                    }                    
-                }
-             
-
-            int value3 = 0;
-            for(int i = wallpaper.Length - 1; i >= 0; i--)
+            for(int i = wallpaper.Length-1; i >= 0; i--)
             {
-                if (wallpaper[i].Contains("#")) break;                
-                else value3++;
+                if (wallpaper[i].Contains("#")) break;
+                else answer3++;                
             }
-
-
             answer[0] = answer1;
             answer[1] = answer2;
-            answer[2] = wallpaper.Length - value3;
+            answer[2] = wallpaper.Length - answer3;
             answer[3] = answer4;
-            
 
             return answer;
     }
