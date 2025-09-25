2e955436413db8e6553b047875ed5706540cd9e2
김범서 <beomkim7@naver.com>
2025-09-25 10:37:24 +0900

    [level 1] Title: 유연근무제, Time: 17.44 ms, Memory: 32.8 MB -BaekjoonHub

--- C#/프로그래머스/1/388351. 유연근무제/유연근무제.cs
+++ C#/프로그래머스/1/388351. 유연근무제/유연근무제.cs
@@ -5,43 +5,39 @@ public int solution(int[] schedules, int[,] timelogs, int startday)
         {
             int answer = 0;
 
-            int length = schedules.Length;
-            
-            for(int i = 0; i < length; i++)
+            for(int i = 0; i < schedules.Length; i++)
             {
+                            int sDay = startday;
+
                 bool check = true;
-                int pixD = startday;
-                DateTime schedule = convertT(schedules[i].ToString()).AddMinutes(10);
-
-                for(int j = 0; j < timelogs.GetLength(1); j++)
+                DateTime sWork = ConverTime(schedules[i].ToString()).AddMinutes(10);
+                for(int j = 0;j < timelogs.GetLength(1); j++)
                 {
-                    if (pixD > 7) pixD = 1;
-
-                    if (pixD == 6 || pixD == 7)
+                    if (sDay > 7) sDay = 1;
+                    if (sDay == 6 || sDay == 7)
                     {
-                        pixD++;
+                        sDay++;
                         continue;
                     }
-                    
-                    DateTime time = convertT(timelogs[i, j].ToString());
-                    if (schedule < time)
+
+                    DateTime getWork = ConverTime(timelogs[i, j].ToString());
+                    if (sWork < getWork)
                     {
                         check = false;
                         break;
                     }
-                    pixD++;
+                    sDay++;
                 }
-
                 if (check) answer++;
-                
+
             }
 
             return answer;
         }
 
-        public DateTime convertT(string s)
+        public DateTime ConverTime(string s)
         {
-            DateTime dt = Convert.ToDateTime(s.ToString().PadLeft(4, '0').Insert(2, ":"));
+            DateTime dt = Convert.ToDateTime(s.PadLeft(4, '0').Insert(2, ":"));
             return dt;
         }
 }
