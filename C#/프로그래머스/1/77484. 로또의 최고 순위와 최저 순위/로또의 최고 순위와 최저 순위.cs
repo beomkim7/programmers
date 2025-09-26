e278cdc0277dde3a97eee2806efefb0583473920
김범서 <beomkim7@naver.com>
2025-09-26 11:39:42 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 1.01 ms, Memory: 31.5 MB -BaekjoonHub

--- C#/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
+++ C#/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
@@ -1,20 +1,34 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 public class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-            int[] answer = new int[2];
+int[] answer = new int[2];
 
-            int rank = 7;
-            int count = lottos.AsEnumerable().Where(x => x == 0).Count();
+            int[] lottosArr = new int[46];
+            int[] winArr = new int[46];
 
-            lottos = lottos.AsEnumerable().OrderByDescending(x => x).ToArray();
-            win_nums = win_nums.AsEnumerable().OrderByDescending(x => x).ToArray();
+            for(int i = 0; i < lottos.Length; i++)
+            {
+                lottosArr[lottos[i]]++;
+                winArr[win_nums[i]]++;
+            }
 
-            for (int i = 0; i < lottos.Length; i++)
-                if (win_nums.Contains(lottos[i])) rank--;
+            int commCnt = 0;
+            for (int i = 0; i <= 45; i++)
+                if (lottosArr[i] == 1 && winArr[i] == 1) commCnt++;
 
-            answer[0] = rank - count==7?6:rank-count;
-            answer[1] = rank==7?6:rank;
+            int bonusCnt = lottos.AsEnumerable().Where(x=>x==0).Count();
+
+            int Cnt = commCnt + bonusCnt;
+            //65432
+            //12345 else6
+
+            if (commCnt == 0) commCnt=1;
+            if (Cnt == 0) Cnt=1;
+
+            answer[1] = 7 - commCnt;
+            answer[0] = 7 - Cnt;
 
             return answer;
     }
