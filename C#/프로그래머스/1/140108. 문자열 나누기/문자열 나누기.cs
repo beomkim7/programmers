ff63f7da2eb02b38e2159904d8c5917e40075957
김범서 <beomkim7@naver.com>
2025-09-26 14:39:07 +0900

    [level 1] Title: 문자열 나누기, Time: 0.41 ms, Memory: 31.4 MB -BaekjoonHub

--- C#/프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cs
+++ C#/프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cs
@@ -2,33 +2,19 @@ using System;
 
 public class Solution {
     public int solution(string s) {
-            int answer = 0;
-
-
+                                    int answer = 0;
 
             for(int i = 0; i < s.Length; i++)
             {
-                char check = s[i];
-                int first = 1;
-                int second = 0;
-
-                for (int j = i + 1; j < s.Length; j++)
+                char c = s[i];
+                int comm = 1;
+                int dif = 0;
+                while (comm>dif)
                 {
-                    if (first == second)
-                    {
-                        break;
-                    }
-
-                    if (check == s[j])
-                    {
-                        first++;
-                        i++;
-                    }
-                    else
-                    {
-                        second++;
-                        i++;
-                    }                    
+                    if (i == s.Length-1) break;
+                    if (c == s[i+1]) comm++;
+                    else dif++;
+                    i++;
                 }
                 answer++;
             }
