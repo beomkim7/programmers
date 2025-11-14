6ec078a66e24a9ac8691b4357f8905dadcadbd30
김범서 <beomkim7@naver.com>
2025-11-14 15:42:14 +0900

    [level 1] Title: 명예의 전당 (1), Time: 14.05 ms, Memory: 33.3 MB -BaekjoonHub

--- C#/프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cs
+++ C#/프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 public class Solution {
     public int[] solution(int k, int[] score) {
-            int[] answer = new int[score.Length];
+int[] answer = new int[score.Length];
+
             List<int> check = new List<int>();
 
-            for (int i = 0; i < score.Length; i++)
+            for(int i = 0; i < score.Length; i++)
             {
                 check.Add(score[i]);
-                check = check.OrderByDescending(x=>x).ToList();
+                check = check.OrderByDescending(x => x).ToList();
                 if (check.Count > k) check.RemoveAt(check.Count - 1);
                 answer[i] = check[check.Count - 1];
             }
-
 
             return answer;
     }
