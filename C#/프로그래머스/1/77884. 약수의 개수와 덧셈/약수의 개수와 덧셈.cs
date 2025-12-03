e18c83861e9cee4a21bbfd52c67d022333ccd0f0
김범서 <beomkim7@naver.com>
2025-12-03 14:58:07 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 1.11 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
+++ C#/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
@@ -1,18 +1,26 @@
 using System;
 
 public class Solution {
-    public int solution(int left, int right) {
+        public int solution(int left, int right)
+        {
             int answer = 0;
-
             for(int i = left; i <= right; i++)
             {
-                int count = 0;
-                for (int j = 1; j <= i; j++)
-                    if (i % j == 0) count++;
-
-                answer = answer + (count % 2 == 0 ? i : -i);
+                if (checkNo(i) % 2 == 0)
+                    answer += i;
+                else answer -= i;
             }
 
             return answer;
-    }
+        }
+
+        public int checkNo(int i)
+        {
+            int result = 0;
+            for (int j = 1; j <= i / 2; j++)            
+                if (i % j == 0) result++;
+
+            result++;
+            return result;
+        }
 }
