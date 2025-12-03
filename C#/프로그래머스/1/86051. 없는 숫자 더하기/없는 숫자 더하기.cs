76b06e74ec9e03503d5235884c846e214dfec807
김범서 <beomkim7@naver.com>
2025-12-03 15:23:13 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.56 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ C#/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+public class Solution {
+    public int solution(int[] numbers) {
+            int answer = 0;
+            for (int i = 1; i <= 9; i++)
+                if (!numbers.Contains(i)) answer += i;
+
+            return answer;
+    }
+}
