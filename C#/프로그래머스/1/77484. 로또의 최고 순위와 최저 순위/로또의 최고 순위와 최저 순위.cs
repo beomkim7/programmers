0e17a0253cba29a4826bcb40772c26c81b20a1d3
김범서 <beomkim7@naver.com>
2025-12-03 14:46:02 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 1.92 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
+++ C#/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
@@ -1,34 +1,21 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 public class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-int[] answer = new int[2];
+            int[] answer = new int[2];
+            int result = 0;            
 
-            int[] lottosArr = new int[46];
-            int[] winArr = new int[46];
-
-            for(int i = 0; i < lottos.Length; i++)
+            for(int i = 0; i < win_nums.Length; i++)
             {
-                lottosArr[lottos[i]]++;
-                winArr[win_nums[i]]++;
+                if (lottos.Contains(win_nums[i])) result++;
             }
 
-            int commCnt = 0;
-            for (int i = 0; i <= 45; i++)
-                if (lottosArr[i] == 1 && winArr[i] == 1) commCnt++;
+            int bonus = lottos.Where(x => x == 0).Count()+result;
 
-            int bonusCnt = lottos.AsEnumerable().Where(x=>x==0).Count();
-
-            int Cnt = commCnt + bonusCnt;
-            //65432
-            //12345 else6
-
-            if (commCnt == 0) commCnt=1;
-            if (Cnt == 0) Cnt=1;
-
-            answer[1] = 7 - commCnt;
-            answer[0] = 7 - Cnt;
+            int max = Math.Min(7 - bonus, 6);
+            int min = Math.Min(7 - result, 6);
+            answer[0] = max;
+            answer[1] = min;
 
             return answer;
     }
