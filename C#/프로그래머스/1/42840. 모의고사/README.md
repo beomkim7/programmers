562393ccd6c319b480a1838c6f85315471afb77b
김범서 <beomkim7@naver.com>
2025-12-03 09:57:17 +0900

    [level 1] Title: 모의고사, Time: 0.94 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/1/42840. 모의고사/README.md
+++ C#/프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 31.2 MB, 시간: 6.47 ms
+메모리: 31.3 MB, 시간: 0.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 30일 16:30:55
+2025년 12월 03일 09:57:12
 
 ### 문제 설명
 
--- C#/프로그래머스/1/42840. 모의고사/모의고사.cs
+++ C#/프로그래머스/1/42840. 모의고사/모의고사.cs
@@ -1,37 +1,31 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-
 public class Solution {
     public int[] solution(int[] answers) {
-            Dictionary<int, int> dict = new Dictionary<int, int>();
+            List<int> answer = new List<int>();
+            int Math1result = 0;
+            int Math2result = 0;
+            int Math3result = 0;
 
-            int[] oneArr = new int[] { 1, 2, 3, 4, 5 };
-            int[] twoArr = new int[] { 2, 1, 2, 3, 2, 4, 2, 5 };
-            int[] thrArr = new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+            int[] math = new int[] { 1, 2, 3, 4, 5 };
+            int[] math2 = new int[] { 2, 1, 2, 3, 2, 4, 2, 5 };
+            int[] math3 = new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
 
-                int one =0;
-                int two =0;
-                int thr = 0;
-
-            for (int i = 0; i < answers.Length; i++)
+            for(int i = 0; i < answers.Length;i++)
             {
-                if (oneArr[i % oneArr.Length] == answers[i]) one++;
-
-                if (twoArr[i % twoArr.Length] == answers[i]) two++;
-                
-                if (thrArr[i % thrArr.Length] == answers[i]) thr++;
-                
+                    if (math[i % 5] ==   answers[i]) Math1result++;
+                    if (math2[i % 8] ==  answers[i]) Math2result++;
+                    if (math3[i % 10] == answers[i]) Math3result++;                
             }
-            dict[0] = one;
-            dict[1] = two;
-            dict[2] = thr;
 
-            int maxV = dict.Values.Max();
+            int maxScore = Math.Max(Math1result, Math.Max(Math2result, Math3result));
+
+            if(maxScore == Math1result) answer.Add(1);
+            if(maxScore == Math2result) answer.Add(2);
+            if(maxScore == Math3result) answer.Add(3);
+            
             
 
-            int[] answer = dict.Where(x=>x.Value==maxV).Select(x=>x.Key+1).ToArray();
-
-            return answer;
+            return answer.ToArray();
     }
 }
