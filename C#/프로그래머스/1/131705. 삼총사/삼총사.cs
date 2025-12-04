75a46984e7dfab177742733aa065cb179f4c68d8
김범서 <beomkim7@naver.com>
2025-12-04 15:14:21 +0900

    [level 1] Title: 삼총사, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/1/131705. 삼총사/삼총사.cs
+++ C#/프로그래머스/1/131705. 삼총사/삼총사.cs
@@ -2,18 +2,12 @@ using System;
 
 public class Solution {
     public int solution(int[] number) {
-            int answer = 0;
-            for(int i = 0; i < number.Length-2; i++)
-            {
-                for(int j = i+1; j < number.Length-1; j++)
-                {
-                    
-                    for(int z = j+1; z < number.Length; z++)
-                    {                        
-                        if (number[i] + number[j] + number[z] == 0) answer++;
-                    }
-                }
-            }
+int answer = 0;
+
+            for (int i = 0; i < number.Length - 2; i++)
+                for (int j = i + 1; j < number.Length - 1; j++)
+                    for (int k = j + 1; k < number.Length; k++)
+                        if (number[i] + number[j] + number[k] == 0) answer++;
 
             return answer;
     }
