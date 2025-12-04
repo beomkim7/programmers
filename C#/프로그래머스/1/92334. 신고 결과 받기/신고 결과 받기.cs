7d9d746dd399f156e6af36d5f55501486a1dbc0d
김범서 <beomkim7@naver.com>
2025-12-04 13:29:17 +0900

    [level 1] Title: 신고 결과 받기, Time: 938.47 ms, Memory: 77.6 MB -BaekjoonHub

--- C#/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
+++ C#/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
@@ -1,36 +1,39 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
 public class Solution {
     public int[] solution(string[] id_list, string[] report, int k) {
-            int length = id_list.Length;
-
-            int[] answer = new int[length];
+int[] answer = new int[id_list.Length];
 
             Dictionary<string, int> dict = new Dictionary<string, int>();
-            Dictionary<string, int> Fcheck = new Dictionary<string, int>();
-            foreach (string s in id_list)
-            {
-                dict[s] = 0;
-                Fcheck[s] = 0;
-            }
-            
-            report = report.AsEnumerable().Distinct().ToArray();
-            
+            report = report.Distinct().ToArray();
             for(int i = 0; i < report.Length; i++)
             {
-                string check = report[i].Split(' ')[1];
-                dict[check]++;
+string stSplit = report[i].Split(' ')[1];
+                if (dict.ContainsKey(stSplit)) dict[stSplit]++;
+                else dict[stSplit] = 1;
             }
 
             for(int i = 0; i < report.Length; i++)
             {
-                string[] check = report[i].Split(' ');
-                if (dict[check[1]] >= k) Fcheck[check[0]]++;
-            }
+                string stSplit = report[i].Split(' ')[0];
+                string stSplit2 = report[i].Split(' ')[1];
 
-            answer = Fcheck.Values.ToArray();
+                if (dict[stSplit2] >= k)
+                {
+                    int no = 0;
+                    for(int j = 0; j < id_list.Length; j++)
+                    {
+                        if (id_list[j] == stSplit)
+                        {
+                            no = j;
+                            break;
+                        }
+                    }
+                    answer[no]++;
+                }
+
+            }
 
             return answer;
     }
