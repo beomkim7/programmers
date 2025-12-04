4658db419931f0cfa25add36a884a4884ebc758b
김범서 <beomkim7@naver.com>
2025-12-05 08:40:06 +0900

    [level 1] Title: 소수 만들기, Time: 73.56 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/1/12977. 소수 만들기/소수 만들기.cs
+++ C#/프로그래머스/1/12977. 소수 만들기/소수 만들기.cs
@@ -3,33 +3,28 @@ using System;
 class Solution
 {
     public int solution(int[] nums)
-    {
-        int answer = 0;
+        {
+                        int answer = 0;
 
-            for(int i = 0; i < nums.Length - 2; i++)
-            {
-                
-                for(int j = i+1;j < nums.Length - 1; j++)
-                {
-                    for(int z = j + 1; z < nums.Length; z++)
+            for(int i = 0; i <nums.Length-2;i++)
+                for(int j = i+1;j<nums.Length-1;j++)
+                    for(int k = j+1; k < nums.Length; k++)
                     {
-                        bool check = true;
-                        int sum = nums[i] + nums[j] + nums[z];
-
-                        for(int num = 2; num <= Math.Sqrt(sum); num++)
-                        {
-                            if (sum % num == 0)
-                            {
-                                check = false;
-                                break;
-                            }                            
-                        }
-                        if (check) answer++;
+                        if (check(nums[i] + nums[j] + nums[k]) == 2) answer++;
                     }
-                }
-                
-            }
+            
 
             return answer;
-    }
+        }
+
+        public int check(int i)
+        {
+            int result = 0;
+            for(int j = 1; j <= i ; j++)
+            {
+                if (i % j == 0) result++;
+            }
+
+            return result;
+        }
 }
