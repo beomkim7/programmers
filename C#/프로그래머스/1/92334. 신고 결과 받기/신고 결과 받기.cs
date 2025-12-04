100df06193f58abe73620f38e192b165ead33c45
김범서 <beomkim7@naver.com>
2025-12-04 16:38:26 +0900

    [level 1] Title: 신고 결과 받기, Time: 1480.55 ms, Memory: 78 MB -BaekjoonHub

--- C#/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
+++ C#/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
@@ -1,38 +1,30 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 public class Solution {
     public int[] solution(string[] id_list, string[] report, int k) {
-int[] answer = new int[id_list.Length];
+            int[] answer = new int[id_list.Length];
 
             Dictionary<string, int> dict = new Dictionary<string, int>();
+
             report = report.Distinct().ToArray();
-            for(int i = 0; i < report.Length; i++)
+            foreach(string s in report)
             {
-string stSplit = report[i].Split(' ')[1];
-                if (dict.ContainsKey(stSplit)) dict[stSplit]++;
-                else dict[stSplit] = 1;
+                string[] sSplit = s.Split(' ');
+                string first = sSplit[1];
+                if (dict.ContainsKey(first)) dict[first]++;
+                else dict[first] = 1;
             }
 
             for(int i = 0; i < report.Length; i++)
             {
-                string stSplit = report[i].Split(' ')[0];
-                string stSplit2 = report[i].Split(' ')[1];
+                string[] sSplit = report[i].Split(' ');
+                string first = sSplit[0];
+                string second = sSplit[1];
 
-                if (dict[stSplit2] >= k)
-                {
-                    int no = 0;
-                    for(int j = 0; j < id_list.Length; j++)
-                    {
-                        if (id_list[j] == stSplit)
-                        {
-                            no = j;
-                            break;
-                        }
-                    }
-                    answer[no]++;
-                }
-
+                int num = Array.IndexOf(id_list, first);
+                if (dict[second] >= k) answer[num]++;
             }
 
             return answer;
