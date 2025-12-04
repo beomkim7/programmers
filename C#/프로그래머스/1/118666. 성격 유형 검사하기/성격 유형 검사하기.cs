96fa6db5a39357e63dcb605d3fb3fd8a0873b956
김범서 <beomkim7@naver.com>
2025-12-04 16:49:03 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 6.14 ms, Memory: 31.5 MB -BaekjoonHub

--- C#/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
+++ C#/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
@@ -1,48 +1,49 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 public class Solution {
-    public string solution(string[] survey, int[] choices) {string answer = "";
-            int length = survey.Length;
+    public string solution(string[] survey, int[] choices) {
+string answer = "";
             Dictionary<string, List<int>> dict = new Dictionary<string, List<int>>();
+
             dict.Add("RT", new List<int> { 0, 0 });
             dict.Add("CF", new List<int> { 0, 0 });
             dict.Add("JM", new List<int> { 0, 0 });
             dict.Add("AN", new List<int> { 0, 0 });
 
-            for (int i = 0; i < length; i++)
-            {
-                int score = Math.Abs(choices[i] - 4);
-
-
-                if (choices[i] > 4)
-                {
-                    char c = survey[i][1];
-                    string check = new string(survey[i].OrderBy(x => x).ToArray());
-                    int num = check.IndexOf(c);
-                    dict[check][num] += score;
-
-                }
-                else if (choices[i] < 4)
+            for(int i = 0; i < survey.Length; i++)
+            {                
+                int value = Math.Abs(choices[i] - 4);
+                if (choices[i] < 4)
                 {
                     char c = survey[i][0];
-                    string check = new string(survey[i].OrderBy(x => x).ToArray());
-                    int num = check.IndexOf(c);
-                    dict[check][num] += score;
+                    string st = new string(survey[i].OrderBy(x => x).ToArray());
+                    int check = st.IndexOf(c);
+
+                    dict[st][check] += value;
+                }
+                else if (choices[i] > 4)
+                {
+                    char c = survey[i][1];
+                    string st = new string(survey[i].OrderBy(x => x).ToArray());
+                    int check = st.IndexOf(c);
+
+                    dict[st][check] += value;
                 }
             }
 
-            foreach (var a in dict)
+            foreach(var v in dict)
             {
-                char first = a.Key[0];
-                char second = a.Key[1];
-                int firstN = a.Value[0];
-                int secondN = a.Value[1];
+                char f = v.Key[0];
+                char s = v.Key[1];
 
-                answer += firstN >= secondN ? first : second;
+                int fNo = v.Value[0];
+                int sNo = v.Value[1];
+
+                answer += fNo >= sNo ? f : s;
             }
+
             return answer;
     }
 }
