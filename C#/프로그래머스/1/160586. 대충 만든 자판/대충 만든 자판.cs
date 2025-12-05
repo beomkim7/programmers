6049a76f43e46a176435e19d7285183c27a2123f
김범서 <beomkim7@naver.com>
2025-12-05 15:25:48 +0900

    [level 1] Title: 대충 만든 자판, Time: 10.23 ms, Memory: 31.7 MB -BaekjoonHub

--- C#/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cs
+++ C#/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cs
@@ -1,29 +1,32 @@
-using System;
-using System.Linq;
+using System;using System.Linq;
+
 public class Solution {
     public int[] solution(string[] keymap, string[] targets) {
-            int[] answer = new int[targets.Length];
+int length = targets.Length;
+            int[] answer = new int[length];
 
-            for(int i = 0; i < targets.Length; i++)
-            {
-                for(int j = 0; j < targets[i].Length; j++)
-                {                    
+            for(int i = 0; i < length; i++)
+            {                
+                for (int j = 0; j < targets[i].Length; j++)
+                {
                     int min = int.MaxValue;
-                    for(int z= 0; z < keymap.Length; z++)
-                    {                        
-                        if (keymap[z].Contains(targets[i][j]))
+                    int num = 0;
+                    for (int k = 0; k < keymap.Length; k++)
+                    {
+                        if (keymap[k].Contains(targets[i][j]))
                         {
-                            int cnt = keymap[z].IndexOf(targets[i][j]);
-                            if(min > cnt) min = cnt;
+                            num = keymap[k].IndexOf(targets[i][j]);
+                            if (min > num) min = num;
                         }
                     }
+                    answer[i] += min + 1;
                     if (min == int.MaxValue)
                     {
                         answer[i] = -1;
                         break;
                     }
-                    else answer[i] += min + 1;
-                }
+                }     
+                
             }
 
             return answer;
