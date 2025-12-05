ff564fc81a47542c898f84b5159208d35575e496
김범서 <beomkim7@naver.com>
2025-12-05 16:44:45 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 1.10 ms, Memory: 31.2 MB -BaekjoonHub

--- C#/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
+++ C#/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
@@ -2,20 +2,31 @@ using System;
 using System.Linq;
 public class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-            int[] answer = new int[2];
-            int result = 0;            
+int[] answer = new int[2];
+            int length = lottos.Length;
 
-            for(int i = 0; i < win_nums.Length; i++)
+            int bonus = 0;
+            int result = 0;
+            for(int i = 0; i < length; i++)
             {
-                if (lottos.Contains(win_nums[i])) result++;
+                for(int j = 0; j < length; j++)
+                {
+                    if (lottos[i] == win_nums[j])
+                    {
+                        result++;
+                        break;
+                    }
+                }
             }
 
-            int bonus = lottos.Where(x => x == 0).Count()+result;
+            bonus = lottos.Where(x => x == 0).Count() + result;
+            //6 5 4 3 2 1 
+            //1 2 3 4 5 6
+            bonus = Math.Min(6, 7- bonus);
+            result = Math.Min(6, 7 - result);
 
-            int max = Math.Min(7 - bonus, 6);
-            int min = Math.Min(7 - result, 6);
-            answer[0] = max;
-            answer[1] = min;
+            answer[0] = bonus;
+            answer[1] = result;
 
             return answer;
     }
