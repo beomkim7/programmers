c4b97bc72dc1f67f34cafc9e6ebb4532a6629862
김범서 <beomkim7@naver.com>
2025-12-10 08:58:29 +0900

    [level 1] Title: 숫자 짝꿍, Time: 29.48 ms, Memory: 59.9 MB -BaekjoonHub

--- C#/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cs
+++ C#/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cs
@@ -1,21 +1,23 @@
 using System;
+using System.Linq;
 using System.Text;
 public class Solution {
     public string solution(string X, string Y) {
-StringBuilder sb = new StringBuilder();
+string answer = "";
+            StringBuilder sb = new StringBuilder();
 
             int[] Xarr = new int[10];
             int[] Yarr = new int[10];
 
-            foreach (char c in X) Xarr[c - '0']++;
-            foreach (char c in Y) Yarr[c - '0']++;
-            
-            for(int i = 9; i >= 0; i--)
-            {
-                int cnt = Math.Min(Xarr[i], Yarr[i]);
+            foreach(char c in X) Xarr[c - '0']++;
+            foreach(char c in Y) Yarr[c - '0']++;
 
-                if (cnt > 0) sb.Append(new string((char)(i + '0'), cnt));
+            for(int i = 9; i >=0; i--)
+            {
+                int check = Math.Min(Xarr[i], Yarr[i]);
+                if (check > 0) sb.Append((char)(i + '0'), check);
             }
+
             if (sb.Length == 0) return "-1";
             if (sb[0] == '0') return "0";
 
