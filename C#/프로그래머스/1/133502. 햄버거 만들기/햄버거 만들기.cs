90b8810ff239f00c1a305a9321e23feb64075e00
김범서 <beomkim7@naver.com>
2025-12-09 17:52:45 +0900

    [level 1] Title: 햄버거 만들기, Time: 281.91 ms, Memory: 61.3 MB -BaekjoonHub

--- C#/프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cs
+++ C#/프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cs
@@ -5,23 +5,22 @@ public class Solution {
     public int solution(int[] ingredient) {
             int answer = 0;
             Stack<int> st = new Stack<int>();
-            
+
             for(int i = 0; i < ingredient.Length; i++)
             {
                 st.Push(ingredient[i]);
-                if (st.Count() >= 4)
+
+                if (st.Count >= 4)
                 {
                     List<int> check = st.Take(4).Reverse().ToList();
                     if (check.SequenceEqual(new List<int> { 1, 2, 3, 1 }))
                     {
-                        answer++;
                         for (int j = 0; j < check.Count(); j++) st.Pop();
+                        answer++;
                     }
-                    
+
                 }
-
             }
-
 
             return answer;
     }
