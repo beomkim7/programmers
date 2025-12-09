20b2dde3d9e2694f70f03f6b003a63c27c6688ab
김범서 <beomkim7@naver.com>
2025-12-09 17:30:18 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.41 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
+++ C#/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
@@ -3,38 +3,31 @@ using System;
 public class Solution {
     public int[] solution(string[] wallpaper) {
 int[] answer = new int[4];
+            int xMin = 0;
+            int yMin = int.MaxValue;
 
-            int answer1 = 0;
-            int answer2 = int.MaxValue;
-            int answer3 = 0;
-            int answer4 = 0;
+            int xMax = 0;
+            int yMax = 0;
 
-            foreach(string s in wallpaper)
+            bool firstV = true;
+            for(int i = 0; i <wallpaper.Length;i++)
             {
-                if (s.Contains("#")) break;
-                answer1++;
-            }
-
-            foreach(string s in wallpaper)
-            {
-                if (s.Contains("#"))
+                int check = wallpaper[i].IndexOf('#');
+                
+                int yMaxcheck = wallpaper[i].LastIndexOf('#')+1;
+                if (check == -1 && firstV)
                 {
-                    int check = s.IndexOf('#');
-                    if (answer2 > check) answer2 = check;
-                    check = s.LastIndexOf("#");
-                    if (answer4 <= check) answer4 = check + 1;
-                }
+                    xMin++;                    
+                }else firstV = false;
+                if (check != -1 && check < yMin) yMin = check;
+                if (check != -1) xMax = i+1;
+                if (check != -1 && yMaxcheck > yMax) yMax = yMaxcheck;
             }
 
-            for(int i = wallpaper.Length-1; i >= 0; i--)
-            {
-                if (wallpaper[i].Contains("#")) break;
-                else answer3++;                
-            }
-            answer[0] = answer1;
-            answer[1] = answer2;
-            answer[2] = wallpaper.Length - answer3;
-            answer[3] = answer4;
+            answer[0] = xMin;
+            answer[1] = yMin;
+            answer[2] = xMax;
+            answer[3] = yMax;
 
             return answer;
     }
