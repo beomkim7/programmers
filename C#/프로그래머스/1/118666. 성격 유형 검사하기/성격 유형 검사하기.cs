e518270bcf86ecf7aa37258e70dfe3a1a27b1de4
김범서 <beomkim7@naver.com>
2025-12-10 09:16:12 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 4.62 ms, Memory: 32.1 MB -BaekjoonHub

--- C#/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
+++ C#/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
@@ -1,34 +1,37 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 public class Solution {
     public string solution(string[] survey, int[] choices) {
-        string answer = "";
+string answer = "";
 
             Dictionary<string, int[]> dict = new Dictionary<string, int[]>();
 
-            dict.Add("RT", new int[2]);
-            dict.Add("CF", new int[2]);
-            dict.Add("JM", new int[2]);
-            dict.Add("AN", new int[2]);
+            dict.Add("RT", new int[2] { 0, 0 });
+            dict.Add("CF", new int[2] { 0, 0 });
+            dict.Add("JM", new int[2] { 0, 0 });
+            dict.Add("AN", new int[2] { 0, 0 });
 
             for(int i = 0; i < survey.Length; i++)
             {
+                int score = Math.Abs(choices[i] - 4);
+
                 if (choices[i] > 4)
                 {
                     char c = survey[i][1];
                     string st = new string(survey[i].OrderBy(x => x).ToArray());
+                    int num = st.IndexOf(c);
 
-                    int cnt = st.IndexOf(c);
-                    dict[st][cnt] += choices[i] - 4;
+                    dict[st][num] += score;
                 }
                 else if (choices[i] < 4)
                 {
                     char c = survey[i][0];
                     string st = new string(survey[i].OrderBy(x => x).ToArray());
+                    int num = st.IndexOf(c);
 
-                    int cnt = st.IndexOf(c);
-                    dict[st][cnt] += 4 - choices[i];
+                    dict[st][num] += score;
                 }
             }
 
@@ -37,11 +40,10 @@ public class Solution {
                 char first = v.Key[0];
                 char second = v.Key[1];
 
-                int firstNo = v.Value[0];
-                int secondNo = v.Value[1];
+                int firstS = v.Value[0];
+                int SecondS = v.Value[1];
 
-                if (firstNo >= secondNo) answer += first;
-                else answer += second;
+                answer += firstS >= SecondS ? first : second;
 
             }
 
