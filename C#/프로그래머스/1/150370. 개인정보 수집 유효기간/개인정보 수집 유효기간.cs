e7888a56b42b6c0cc6d0154542201c67becf075b
김범서 <beomkim7@naver.com>
2025-12-10 10:38:38 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 11.58 ms, Memory: 31.9 MB -BaekjoonHub

--- C#/프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cs
+++ C#/프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cs
@@ -1,38 +1,32 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
 public class Solution {
     public int[] solution(string today, string[] terms, string[] privacies)
         {
-            List<int> answer = new List<int>();
+            int[] answer = new int[] { };
+            List<int> answerArr = new List<int>();
 
-            DateTime Dtoday = ConvertD(today);
+            Dictionary<string, DateTime> dict = new Dictionary<string, DateTime>();
 
             for(int i = 0; i < terms.Length; i++)
             {
-                string[] termArr = terms[i].Split(' ');
-
-                for(int j = 0; j < privacies.Length; j++)
-                {
-                    if (privacies[j].Contains(termArr[0]))
-                    {
-                        string[] priArr = privacies[j].Split(' ');
-                        DateTime Edt = ConvertD(priArr[0]);
-                        Edt = Edt.AddMonths(int.Parse(termArr[1]));
-
-                        if (Edt <= Dtoday) answer.Add(j + 1);
-                    }
-                }
-
+                string[] parseT = terms[i].Split(' ');
+                dict.Add(parseT[0], ConverT(today).AddMonths(-int.Parse(parseT[1])));
             }
 
+            for(int i = 0; i < privacies.Length; i++)
+            {
+                string[] parseT = privacies[i].Split(' ');
+                DateTime priDt = ConverT(parseT[0]);
 
-            return answer.OrderBy(x=>x).ToArray();
+                if (dict[parseT[1]] >= priDt) answerArr.Add(i+1);
+            }
+
+            return answerArr.ToArray();
         }
-
-        public DateTime ConvertD(string s)
+        public DateTime ConverT(string s)
         {
-            DateTime dt = Convert.ToDateTime(s.Replace('.','/'));
+            DateTime dt = Convert.ToDateTime(s);
             return dt;
         }
 }
