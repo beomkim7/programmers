9930f04a9e66f44697ad328983c31918b31ff28f
김범서 <beomkim7@naver.com>
2025-12-10 10:01:12 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.41 ms, Memory: 31.4 MB -BaekjoonHub

--- C#/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
+++ C#/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
@@ -2,32 +2,33 @@ using System;
 
 public class Solution {
     public int[] solution(string[] wallpaper) {
-int[] answer = new int[4];
-            int xMin = 0;
-            int yMin = int.MaxValue;
+        int[] answer = new int[4];
 
-            int xMax = 0;
-            int yMax = 0;
+            int minX = int.MaxValue;
+            int minY = int.MaxValue;
 
-            bool firstV = true;
-            for(int i = 0; i <wallpaper.Length;i++)
+            int maxX = 0;
+            int maxY = 0;
+
+            for(int i = 0; i < wallpaper.Length; i++)
             {
-                int check = wallpaper[i].IndexOf('#');
-                
-                int yMaxcheck = wallpaper[i].LastIndexOf('#')+1;
-                if (check == -1 && firstV)
+                int start = wallpaper[i].IndexOf('#');
+                int end = wallpaper[i].LastIndexOf('#');
+
+                if(start != -1)
                 {
-                    xMin++;                    
-                }else firstV = false;
-                if (check != -1 && check < yMin) yMin = check;
-                if (check != -1) xMax = i+1;
-                if (check != -1 && yMaxcheck > yMax) yMax = yMaxcheck;
+                    minX = Math.Min(minX, i);
+                    minY = Math.Min(minY, start);
+
+                    maxX = Math.Max(maxX, i + 1);
+                    maxY = Math.Max(maxY, end + 1);
+                }
             }
 
-            answer[0] = xMin;
-            answer[1] = yMin;
-            answer[2] = xMax;
-            answer[3] = yMax;
+            answer[0] = minX;
+            answer[1] = minY;
+            answer[2] = maxX;
+            answer[3] = maxY;
 
             return answer;
     }
