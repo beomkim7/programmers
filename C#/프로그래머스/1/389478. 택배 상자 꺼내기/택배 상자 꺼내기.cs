7441d965d5d4197e79028346187d6084a4d2dbdb
김범서 <beomkim7@naver.com>
2025-12-10 16:24:02 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.23 ms, Memory: 31.4 MB -BaekjoonHub

--- C#/프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cs
+++ C#/프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cs
@@ -1,75 +1,46 @@
 using System;
 
-public class Solution 
-{
-    public int solution(int n, int w, int num) 
-    {
-        int answer = 1;
-        int currentNum = num; // 현재 꺼내려는 상자의 번호
+public class Solution {
+    public int solution(int n, int w, int num) {
+int answer = 1;
+            int currentNum = num;
 
-        // 총 층 수 (0부터 시작)를 넘지 않을 때까지 반복
-        while (true)
-        {
-            // 1. 현재 상자(currentNum)의 층(row)과 열(col) 인덱스 계산
-            int currentRow = (currentNum - 1) / w; 
+            while (true)
+            {
+                int currentRow = (currentNum - 1) / w; //12 
+                int num_col;
+                if(currentRow % 2 == 0)
+                {
+                    num_col = (currentNum - 1) % w;
+                }
+                else
+                {
+                    num_col = w - 1 - ((currentNum - 1) % w); //054321 => 012345
+                }
 
-            // (num_col은 왼쪽에서부터 0 ~ w-1)
-            int num_col; 
-            if (currentRow % 2 == 0) // 짝수 층 (0, 2, ...): 왼쪽 -> 오른쪽
-            {
-                num_col = (currentNum - 1) % w;
-            }
-            else // 홀수 층 (1, 3, ...): 오른쪽 -> 왼쪽
-            {
-                num_col = w - 1 - ((currentNum - 1) % w);
+                int nextRow = currentRow + 1;
+                int startNum = nextRow * w +1;
+
+                int nextNum;
+
+                if(nextRow %2 == 0)
+                {
+                    nextNum = startNum + num_col;
+                }
+                else
+                {
+                    nextNum = startNum + (w - 1 - num_col);    //6-1-      17
+                }
+
+                if (nextNum > n) break;
+                else
+                {
+                    // 위에 상자가 존재하면 (치워야 할 상자)
+                    answer++;
+                    currentNum = nextNum; // 다음으로 치워야 할 상자 번호로 갱신
+                }
             }
 
-            // 2. 다음 층(aboveRow)의 상자 번호(nextNum) 계산
-            int aboveRow = currentRow + 1;
-            
-            // 다음 층의 첫 번째 상자 번호
-            int startNum = aboveRow * w + 1; 
-
-            int nextNum;
-            if (aboveRow % 2 == 0) // 다음 층이 짝수 층 (왼쪽 -> 오른쪽)
-            {
-                nextNum = startNum + num_col;
-            }
-            else // 다음 층이 홀수 층 (오른쪽 -> 왼쪽)
-            {
-                nextNum = startNum + (w - 1 - num_col);
-            }
-
-            // 3. 종료 조건 확인 및 갱신
-            if (nextNum > n)
-            {
-                // 위에 쌓인 상자의 번호가 총 상자 개수 n을 초과하면 종료
-                break;
-            }
-            else
-            {
-                // 위에 상자가 존재하면 (치워야 할 상자)
-                answer++;
-                currentNum = nextNum; // 다음으로 치워야 할 상자 번호로 갱신
-            }
-        }
-        
-        // 문제에서 꺼내려는 상자(num)도 치워야 하는 상자 개수에 포함하는지는 명확하지 않으나,
-        // 일반적인 스택/더미 문제와 달리 "꺼내야 하는 상자의 총개수"가 num을 포함하는 예시가 있음.
-        // 하지만 위의 로직은 'num 위에 있는' 상자만 세었으므로, num 자신을 포함해야 한다면 +1이 필요합니다.
-        // 문제 예시 1 (n=22, w=6, num=8)의 답이 3이라는 점을 감안하면, num 자신도 포함됩니다.
-        // (8 위에 20, 32... 20만 존재. 따라서 8 위에 20. 답은 8, 20 포함하여 2개?)
-        // 잠시만, 예시 1의 '8번 상자를 포함해 3개'라는 부분은 [치워야 할 상자 + num 자신]을 의미하는 것이 아니라,
-        // 치워야 할 상자의 개수를 세는 것 같습니다. 다시 문제의 조건을 확인해야 합니다.
-        
-        // 문제 예시를 다시 분석하면, "꺼내야 하는 상자의 총개수"는 'num을 꺼내기 위해 num 위에 있는 모든 상자'의 개수입니다.
-        // 예시 1: 8번 상자를 꺼내려면 20번 상자만 치우면 됨. (답은 1개)
-        // **프로그래머스 실제 문제 조건 확인 결과:** num 위에 있는 상자만 세는 것이 맞습니다.
-        // 예시 1 (n=22, w=6, num=8)의 답은 1입니다. (8 위에 20)
-        // 예시 2 (n=14, w=5, num=6)의 답은 3입니다. (6 위에 13, 12, 7 -> 7, 12, 13이 치워야 할 상자)
-        // 답이 3이 나오려면, 6 위에 13, 12, 7이 아니라, 6 위에 12가 있고 12 위에 13이 있는 것으로 해석해야 합니다. 
-        // 다시 로직을 적용하면, 6번 상자의 위에 있는 상자들만 세는 것이 맞습니다.
-        
-        return answer;
+            return answer;
     }
 }
