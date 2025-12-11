e7c782fc4a3858815c995f06d24040ec208bff83
김범서 <beomkim7@naver.com>
2025-12-11 13:56:39 +0900

    [level 1] Title: 신고 결과 받기, Time: 1019.11 ms, Memory: 77.8 MB -BaekjoonHub

--- C#/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
+++ C#/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
@@ -1,32 +1,30 @@
-using System;
-using System.Collections.Generic;
+using System;using System.Collections.Generic;
 using System.Linq;
 
 public class Solution {
     public int[] solution(string[] id_list, string[] report, int k) {
-            int[] answer = new int[id_list.Length];
-
+int[] answer = new int[id_list.Length];
             Dictionary<string, int> dict = new Dictionary<string, int>();
 
             for (int i = 0; i < id_list.Length; i++) dict[id_list[i]] = 0;
 
             report = report.Distinct().ToArray();
-
             for(int i = 0; i < report.Length; i++) dict[report[i].Split(' ')[1]]++;
+            
 
-            for (int i = 0; i < report.Length; i++)
+            for(int i = 0; i < report.Length; i++)
             {
-                string[] parseR = report[i].Split(' ');
+                string[] reportArr = report[i].Split(' ');
+                string user = reportArr[0];
+                string target = reportArr[1];
 
-                if(dict[parseR[1]] >= k)
+                if (dict[target] >= k)
                 {
-                    int check = Array.IndexOf(id_list, parseR[0]);
-                    answer[check]++;
+                    int Rowcount = Array.IndexOf(id_list, user);
+                    answer[Rowcount]++;
                 }
-
             }
 
-
-                return answer;
+            return answer;
     }
 }
