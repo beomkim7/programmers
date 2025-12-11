8a883a394156346eac69221f86a3ee89b32d8b5c
김범서 <beomkim7@naver.com>
2025-12-11 17:42:13 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.30 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cs
+++ C#/프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cs
@@ -2,30 +2,30 @@ using System;
 
 public class Solution {
     public int solution(int n, int w, int num) {
-            int answer = 1;
-
+int answer = 1;
             int currentNum = num;
 
             while (true)
             {
                 int currentRow = (currentNum - 1) / w;
 
-                int currentPlus;
-                if (currentRow % 2 == 0) currentPlus = (currentNum - 1) % w;
-                else currentPlus = w - 1 - ((currentNum - 1) % w);//8 = 17
+                int row_num;
+                if (currentRow % 2 == 0) row_num = (currentNum - 1) % w;
+                else row_num = w - 1 - ((currentNum - 1) % w);
 
                 int nextRow = currentRow + 1;
                 int nextNum = nextRow * w + 1;
 
-                if (nextRow % 2 == 0) nextNum += currentPlus;
-                else nextNum += w - 1 - currentPlus;
+                if (nextRow % 2 == 0) nextNum += row_num;
+                else nextNum += w - 1 - row_num;
 
                 if (nextNum > n) break;
                 else
                 {
-                    answer++;
                     currentNum = nextNum;
+                    answer++;
                 }
+
             }
 
             return answer;
