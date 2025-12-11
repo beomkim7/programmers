0c82368945a8fb368da242b502fb985613de74e3
김범서 <beomkim7@naver.com>
2025-12-11 15:35:47 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.04 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cs
+++ C#/프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cs
@@ -4,12 +4,10 @@ public class Solution {
 public string solution(string video_len, string pos, string op_start, string op_end, string[] commands)
         {
             string answer = "";
-
-            int len = ConvertToInt(video_len);
-            int now = ConvertToInt(pos);
-            int start = ConvertToInt(op_start);
-            int end = ConvertToInt(op_end);
-
+            int len = convertToSec(video_len);
+            int now = convertToSec(pos);
+            int start = convertToSec(op_start);
+            int end = convertToSec(op_end);
             if (now >= start && now < end) now = end;
 
             foreach (string s in commands)
@@ -17,34 +15,28 @@ public string solution(string video_len, string pos, string op_start, string op_
                 if (s == "next")
                 {
                     now += 10;
-                    if (now > len) now = len;
-                    if (now >= start && now < end) now = end;
+                    now = result(len, now, start, end);                    
                 }
                 else
                 {
                     now -= 10;
-                    if (now <0) now = 0;
-                    if (now >= start && now < end) now = end;
+                    now = result(len, now, start, end);
                 }
             }
-
-            answer = ConvertToString(now);
-
+            answer = ((now / 60).ToString().PadLeft(2, '0') + (now % 60).ToString().PadLeft(2, '0')).Insert(2, ":");
             return answer;
         }
-
-        public int ConvertToInt(string s)
-        {
-            string[] sArr = s.Split(':');
-
-            return int.Parse(sArr[0]) * 60 + int.Parse(sArr[1]);
+        public int result(int len,int now,int start,int end)
+        {            
+            if (now < 0) now = 0;
+            if (now > len) now = len;
+            if (now >= start && now < end) now = end;
+            return now;
         }
 
-        public string ConvertToString(int i)
+        public int convertToSec(string s)
         {
-            string min = (i / 60).ToString().PadLeft(2, '0');
-            string sec = (i % 60).ToString().PadLeft(2, '0');
-            string s = min+sec ;
-            return s.Insert(2, ":");
+            string[] sParse = s.Split(':');
+            return int.Parse(sParse[0]) * 60 + int.Parse(sParse[1]);
         }
 }
