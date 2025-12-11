92dc1b5e9c6801123062083c42aec9af60ae0319
김범서 <beomkim7@naver.com>
2025-12-11 16:32:05 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.31 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cs
+++ C#/프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cs
@@ -2,42 +2,29 @@ using System;
 
 public class Solution {
     public int solution(int n, int w, int num) {
-int answer = 1;
+            int answer = 1;
+
             int currentNum = num;
 
             while (true)
             {
-                int currentRow = (currentNum - 1) / w; //12 
-                int num_col;
-                if(currentRow % 2 == 0)
-                {
-                    num_col = (currentNum - 1) % w;
-                }
-                else
-                {
-                    num_col = w - 1 - ((currentNum - 1) % w); //054321 => 012345
-                }
+                int currentRow = (currentNum - 1) / w;
+
+                int currentPlus;
+                if (currentRow % 2 == 0) currentPlus = (currentNum - 1) % w;
+                else currentPlus = w - 1 - ((currentNum - 1) % w);//8 = 17
 
                 int nextRow = currentRow + 1;
-                int startNum = nextRow * w +1;
+                int nextNum = nextRow * w + 1;
 
-                int nextNum;
-
-                if(nextRow %2 == 0)
-                {
-                    nextNum = startNum + num_col;
-                }
-                else
-                {
-                    nextNum = startNum + (w - 1 - num_col);    //6-1-      17
-                }
+                if (nextRow % 2 == 0) nextNum += currentPlus;
+                else nextNum += w - 1 - currentPlus;
 
                 if (nextNum > n) break;
                 else
                 {
-                    // 위에 상자가 존재하면 (치워야 할 상자)
                     answer++;
-                    currentNum = nextNum; // 다음으로 치워야 할 상자 번호로 갱신
+                    currentNum = nextNum;
                 }
             }
 
