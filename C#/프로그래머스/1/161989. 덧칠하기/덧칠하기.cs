0d300d2320cff86d45cf430a56e0c00a75ca11f1
김범서 <beomkim7@naver.com>
2026-01-05 16:50:03 +0900

    [level 1] Title: 덧칠하기, Time: 0.48 ms, Memory: 31.5 MB -BaekjoonHub

--- C#/프로그래머스/1/161989. 덧칠하기/덧칠하기.cs
+++ C#/프로그래머스/1/161989. 덧칠하기/덧칠하기.cs
@@ -2,21 +2,18 @@ using System;
 
 public class Solution {
     public int solution(int n, int m, int[] section) {
-            int answer = 0;
+int answer = 0;
+
             int index = 0;
 
-            while (index < section.Length)
+            while(index < section.Length)
             {
                 int start = section[index];
                 int end = start + m - 1;
-                
-                while (index < section.Length && section[index] <= end)
-                    index++;
 
-
+                while (index < section.Length && section[index] <= end) index++;
                 answer++;
             }
-
 
             return answer;
     }
