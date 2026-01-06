b5fbb18fcf3d0c158602f6586f3ee05af7ecac07
김범서 <beomkim7@naver.com>
2026-01-06 17:08:28 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.64 ms, Memory: 31.2 MB -BaekjoonHub

--- C#/프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cs
+++ C#/프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cs
@@ -6,13 +6,12 @@ class Solution
    {
             int answer = n;
 
-            string nCheck = Convert.ToString(n, 2);
-            int nNum = nCheck.Count(x=>x=='1');
+            int CheckN = Convert.ToString(n, 2).Count(x => x == '1');
             while (true)
             {
-                answer+=1;
-                int answerNum = Convert.ToString(answer, 2).Count(x => x == '1');
-                if (answerNum == nNum) break;
+                answer += 1;
+                int answerN = Convert.ToString(answer, 2).Count(c => c == '1');
+                if (CheckN == answerN) break;
             }
 
             return answer;
