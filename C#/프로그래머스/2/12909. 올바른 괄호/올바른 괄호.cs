e41217aa7aa9f4c92d8d8137fbf96bb9e0b733e5
김범서 <beomkim7@naver.com>
2026-01-06 17:05:16 +0900

    [level 2] Title: 올바른 괄호, Time: 1.48 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cs
+++ C#/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cs
@@ -2,18 +2,18 @@ using System;
 using System.Collections.Generic;
 public class Solution {
     public bool solution(string s) {
+bool answer = true;
             Stack<char> st = new Stack<char>();
+
             foreach(char c in s)
             {
                 if (c == '(') st.Push(c);
                 else
                 {
                     if (st.Count == 0) return false;
-                    st.Pop();
-                }                
+                    else st.Pop();
+                }
             }
-
-            bool answer = st.Count==0;
-            return answer;
+            return answer = st.Count==0;
     }
 }
