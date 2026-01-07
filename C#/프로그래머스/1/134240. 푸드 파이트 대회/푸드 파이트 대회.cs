2c1b121e8b726411e87f286ec8df24b104a0525e
김범서 <beomkim7@naver.com>
2026-01-07 10:07:21 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 6.23 ms, Memory: 34.9 MB -BaekjoonHub

--- C#/프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
+++ C#/프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
@@ -3,15 +3,21 @@ using System.Linq;
 using System.Text;
 public class Solution {
     public string solution(int[] food) {
-            StringBuilder answer = new StringBuilder();
-
-            for (int i = 1; i < food.Length; i++)
-                for (int j = 0; j < food[i] / 2; j++)
-                    answer.Append(i);
-            
-            string reverse = new string(answer.ToString().Reverse().ToArray());
-            answer.AppendFormat("0{0}", reverse);
-
-            return answer.ToString();
+                        string answer = "";
+            for(int i = 1; i < food.Length; i++)
+            {
+                int cnt = 0;
+                if (food[i] == 1) continue;
+                else
+                {
+                    cnt = food[i] / 2;
+                }
+                for (int j = 1; j <= cnt; j++) answer += i;
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.Append(answer.Reverse().ToArray());
+            answer += "0";
+            answer += sb.ToString();
+            return answer;
     }
 }
