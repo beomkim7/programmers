00dc146eec0a576c0109db43ed224c97a459873f
김범서 <beomkim7@naver.com>
2026-01-07 10:22:42 +0900

    [level 1] Title: 과일 장수, Time: 421.65 ms, Memory: 69.9 MB -BaekjoonHub

--- C#/프로그래머스/1/135808. 과일 장수/과일 장수.cs
+++ C#/프로그래머스/1/135808. 과일 장수/과일 장수.cs
@@ -1,21 +1,21 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 public class Solution {
     public int solution(int k, int m, int[] score) {
             int answer = 0;
-            score = score.OrderByDescending(x => x).ToArray();
 
-            for(int i = 0; i < score.Length / m; i++)
+            int box = score.Length / m;
+            Queue<int> que = new Queue<int>();
+            score = score.OrderByDescending(x=>x).ToArray();
+
+            for (int i = 0; i < score.Length; i++) que.Enqueue(score[i]);
+            for(int i = 1; i <= box; i++)
             {
-                int[] check = new int[m];
-                for(int j = 0; j < m; j++)
-                {
-                    check[j] = score[i * m + j];
-                }
-                answer += check[m-1] * m;
+                int[] boxScore = new int[m];
+                for (int j = 1; j <= m; j++) boxScore[j - 1] = que.Dequeue();
+                answer += boxScore[boxScore.Length - 1] * m;
             }
-
-
             return answer;
     }
 }
