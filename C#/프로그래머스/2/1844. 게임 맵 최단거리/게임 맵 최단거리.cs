ddb4ce19a48fd464b978603a520f75a7fd0f40c1
김범서 <beomkim7@naver.com>
2026-01-14 16:56:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 1.20 ms, Memory: 31.8 MB -BaekjoonHub

--- C#/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cs
+++ C#/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cs
@@ -1,22 +1,22 @@
 using System;
 using System.Collections.Generic;
-
 class Solution {
     public int solution(int[,] maps) {
-int answer = 0;
+            int answer = 0;
 
             int n = maps.GetLength(0);
             int m = maps.GetLength(1);
 
-            int[] dx = new int[] { -1, 1, 0, 0 };
-            int[] dy = new int[] { 0, 0 , -1, 1};
+            int[] dx = { -1, 1, 0, 0 };
+            int[] dy = {  0, 0, -1, 1 };
 
-            Queue<int[]> que = new Queue<int[]>();
-            que.Enqueue(new int[] { 0, 0 });
+            Queue<int[]> que = new Queue<int[]> { };
+            que.Enqueue(new int[]{ 0,0});
 
             while (que.Count > 0)
             {
                 int[] check = que.Dequeue();
+
                 int x = check[0];
                 int y = check[1];
 
@@ -24,19 +24,16 @@ int answer = 0;
                 {
                     int nx = x + dx[i];
                     int ny = y + dy[i];
-
-                    if(nx>=0 && nx<n && ny >= 0 && ny < m)
-                    {
-                        if (maps[nx, ny] == 1)
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < m)
+                        if(maps[nx,ny]==1)
                         {
-                            maps[nx, ny] = maps[x, y] + 1;
+                            maps[nx, ny] = maps[x,y]+1;
                             que.Enqueue(new int[] { nx, ny });
                         }
-                    }
                 }
             }
-
             answer = maps[n - 1, m - 1];
-            return answer == 1 ? -1 : answer;
+
+            return answer == 1 ? -1:answer;
     }
 }
