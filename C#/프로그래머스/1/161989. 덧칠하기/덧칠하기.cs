f4b861949c246cb1b8d277886a7afb887040b334
김범서 <beomkim7@naver.com>
2026-01-14 16:34:18 +0900

    [level 1] Title: 덧칠하기, Time: 0.49 ms, Memory: 32 MB -BaekjoonHub

--- C#/프로그래머스/1/161989. 덧칠하기/덧칠하기.cs
+++ C#/프로그래머스/1/161989. 덧칠하기/덧칠하기.cs
@@ -5,14 +5,15 @@ public class Solution {
             int answer = 1;
             int num = 0;
             int check = section[num] + m - 1;
-            while (num<=section.Length-1)
-            {                
+
+            while(num <= section.Length-1)
+            {
                 if(check < section[num])
                 {
                     answer++;
                     check = section[num] + m - 1;
                 }
-                num++;
+                num += 1;
             }
 
             return answer;
