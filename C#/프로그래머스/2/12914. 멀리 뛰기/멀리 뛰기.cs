aa9de9f1bc56447786834a9d62cc5443b05fc22c
김범서 <beomkim7@naver.com>
2026-01-15 16:16:25 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.31 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cs
+++ C#/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cs
@@ -1,17 +1,17 @@
 public class Solution {
     public long solution(int n) {
-                                             if (n == 1) return 1;
+            if (n == 1) return 1;
             if (n == 2) return 2;
             long answer = 0;
-            long[] nArr = new long[n+1];
+            int[] nArr = new int[n+1];
             nArr[1] = 1;
             nArr[2] = 2;
-            if (n >= 3)
+
+            for(int i = 3; i <= n; i++)
             {
-                for (int i = 3; i <= n; i++)
-                    nArr[i] = (nArr[i - 1] + nArr[i - 2])% 1234567;
+                nArr[i] = (nArr[i - 1] + nArr[i - 2]) % 1234567;
             }
-            answer = nArr[n];
+            answer = nArr[n] ;
             return answer;
     }
 }
