e9c71b7834030be6be56c58e07733f3ddd158f01
김범서 <beomkim7@naver.com>
2026-01-15 17:00:27 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.65 ms, Memory: 32.4 MB -BaekjoonHub

--- C#/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cs
+++ C#/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cs
@@ -2,18 +2,20 @@ using System;
 
 public class Solution {
     public int[,] solution(int[,] arr1, int[,] arr2) {
-            int row1 = arr1.GetLength(0);
-            int col1 = arr1.GetLength(1);
-            int col2 = arr2.GetLength(1);
+int arr1ga = arr1.GetLength(0);
+            int arr1se = arr1.GetLength(1);
+            int arr2se = arr2.GetLength(1);
+            Console.WriteLine(arr1ga);
+            Console.WriteLine(arr1se);
 
-            int[,] answer = new int[row1,col2];
-            for(int i = 0; i<row1;i++)
-                for(int j = 0; j <col2;j++)
-                    for(int z = 0; z < col1; z++)
+            int[,] answer = new int[arr1ga, arr2se];
+
+            for(int i = 0; i < arr1ga; i++)
+                for(int j = 0; j < arr2se;j++)
+                    for(int z= 0; z < arr1se; z++)
                     {
                         answer[i, j] += arr1[i, z] * arr2[z, j];
                     }
-
 
             return answer;
     }
