ba89f944560602ba2f9a5d9014fa18f0c138a8c8
김범서 <beomkim7@naver.com>
2026-01-15 16:36:15 +0900

    [level 2] Title: 피보나치 수, Time: 1.43 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/2/12945. 피보나치 수/피보나치 수.cs
+++ C#/프로그래머스/2/12945. 피보나치 수/피보나치 수.cs
@@ -1,18 +1,15 @@
 public class Solution {
     public int solution(int n) {
+                        if (n == 1) return 1;
+            if (n == 2) return 1;
             int answer = 0;
             int[] nArr = new int[n + 1];
             nArr[1] = 1;
             nArr[2] = 1;
-            if (n == 2) return 1;
-            else
+            for(int i = 3; i <= n; i++)
             {
-                for(int i = 3; i <= n; i++)
-                {
-                    nArr[i] = (nArr[i - 1] + nArr[i - 2]) % 1234567;
-                }
+                nArr[i] = (nArr[i - 1] + nArr[i - 2])%1234567;
             }
-
             answer = nArr[n];
             return answer;
     }
