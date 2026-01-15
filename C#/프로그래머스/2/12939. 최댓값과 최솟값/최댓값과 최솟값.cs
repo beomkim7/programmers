039bf985c39704e87c086148a2dcf1e7f44dfaca
김범서 <beomkim7@naver.com>
2026-01-15 16:20:08 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 2.50 ms, Memory: 30.9 MB -BaekjoonHub

--- C#/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cs
+++ C#/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cs
@@ -3,10 +3,13 @@ public class Solution {
     public string solution(string s) {
             string answer = "";
             string[] sArr = s.Split(' ');
+            int[] nArr = new int[sArr.Length];
+            for (int i = 0; i < sArr.Length; i++) 
+                nArr[i] = int.Parse(sArr[i]);
 
-            sArr = sArr.OrderByDescending(x => int.Parse(x)).ToArray();
-
-            answer = string.Join(" ", sArr[sArr.Length-1], sArr[0]);
+            string max = nArr.Max().ToString();
+            string min = nArr.Min().ToString();
+            answer = string.Join(" ", min, max);
             return answer;
     }
 }
