98de9540ec5a3bdc4e742918350ba5dad674e54b
김범서 <beomkim7@naver.com>
2026-01-16 14:49:17 +0900

    [level 2] Title: 하노이의 탑, Time: 2.07 ms, Memory: 40.3 MB -BaekjoonHub

--- C#/프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cs
+++ C#/프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cs
@@ -1,22 +1,21 @@
 using System;
 using System.Collections.Generic;
 public class Solution {
-public List<int[]> result = new List<int[]>();
+        private List<int[]> result= new List<int[]>();
         public int[][] solution(int n)
         {
             Hanoi(n, 1, 3, 2);
-
             return result.ToArray();
         }
-        //2 [1,2], [1,3], [2,3] 
-        public void Hanoi(int n,int from,int to, int via)
+        //[1,2] , [1,3] , [2,3]
+        public void Hanoi(int num,int from,int to, int via)
         {
-            if (n == 1) result.Add(new int[] { from, to });
+            if (num == 1) result.Add(new int[] { from, to });
             else
             {
-                Hanoi(n - 1, from, via, to);
+                Hanoi(num - 1, from, via, to);
                 result.Add(new int[] { from, to });
-                Hanoi(n - 1, via, to, from);
+                Hanoi(num - 1, via, to,from);
             }
         }
 }
