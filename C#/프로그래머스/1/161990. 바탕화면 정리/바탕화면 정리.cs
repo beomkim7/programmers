1008a3d72935b9338a8964e65940582f618c1a1e
김범서 <beomkim7@naver.com>
2026-02-05 14:13:59 +0900

    [level 1] Title: 바탕화면 정리, Time: 5.28 ms, Memory: 31.6 MB -BaekjoonHub

--- C#/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
+++ C#/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
@@ -2,33 +2,45 @@ using System;
 
 public class Solution {
     public int[] solution(string[] wallpaper) {
-        int[] answer = new int[4];
+int[] answer = new int[4];
 
-            int minX = int.MaxValue;
-            int minY = int.MaxValue;
+            int start = 0;
+            int second = int.MaxValue;
+            int third = 0;
+            int forth = 0;
 
-            int maxX = 0;
-            int maxY = 0;
+            for (int i = 0; i < wallpaper.Length; i++)            
+                if (wallpaper[i].Contains("#"))
+                {
+                    start = i;
+                    break;
+                }
 
             for(int i = 0; i < wallpaper.Length; i++)
             {
-                int start = wallpaper[i].IndexOf('#');
-                int end = wallpaper[i].LastIndexOf('#');
-
-                if(start != -1)
+                int check = int.MaxValue;
+                int check2 = 0;
+                if (wallpaper[i].Contains("#"))
                 {
-                    minX = Math.Min(minX, i);
-                    minY = Math.Min(minY, start);
-
-                    maxX = Math.Max(maxX, i + 1);
-                    maxY = Math.Max(maxY, end + 1);
+                    check = wallpaper[i].IndexOf("#");
+                    check2 = wallpaper[i].LastIndexOf("#");
                 }
-            }
+                if (second > check) second = check;
+                if (forth < check2) forth = check2;
+                
 
-            answer[0] = minX;
-            answer[1] = minY;
-            answer[2] = maxX;
-            answer[3] = maxY;
+            }
+forth += 1;
+            for (int i = wallpaper.Length - 1; i >= 0; i--) 
+                if (wallpaper[i].Contains("#"))
+                {
+                    third = i+1;
+                    break;
+                }
+            answer[0] = start;
+            answer[1] = second;
+            answer[2] = third;
+            answer[3] = forth;
 
             return answer;
     }
