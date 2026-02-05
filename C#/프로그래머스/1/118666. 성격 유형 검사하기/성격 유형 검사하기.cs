2c5b45f94fcf562056ef53c7244c4407b4b1d475
김범서 <beomkim7@naver.com>
2026-02-05 13:28:29 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 6.10 ms, Memory: 31.1 MB -BaekjoonHub

--- C#/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
+++ C#/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
@@ -1,50 +1,35 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
 public class Solution {
     public string solution(string[] survey, int[] choices) {
 string answer = "";
-
             Dictionary<string, int[]> dict = new Dictionary<string, int[]>();
-
-            dict.Add("RT", new int[2] { 0, 0 });
-            dict.Add("CF", new int[2] { 0, 0 });
-            dict.Add("JM", new int[2] { 0, 0 });
-            dict.Add("AN", new int[2] { 0, 0 });
+            dict.Add("RT", new int[] { 0, 0 });
+            dict.Add("CF", new int[] { 0, 0 });
+            dict.Add("JM", new int[] { 0, 0 });
+            dict.Add("AN", new int[] { 0, 0 });
 
             for(int i = 0; i < survey.Length; i++)
             {
+                int index = 0;
+                if (choices[i] > 4) index = 1;
                 int score = Math.Abs(choices[i] - 4);
 
-                if (choices[i] > 4)
-                {
-                    char c = survey[i][1];
-                    string st = new string(survey[i].OrderBy(x => x).ToArray());
-                    int num = st.IndexOf(c);
+                char c = survey[i][index];
+                string st = new string(survey[i].OrderBy(x => x).ToArray());
+                index = st.IndexOf(c);
 
-                    dict[st][num] += score;
-                }
-                else if (choices[i] < 4)
-                {
-                    char c = survey[i][0];
-                    string st = new string(survey[i].OrderBy(x => x).ToArray());
-                    int num = st.IndexOf(c);
-
-                    dict[st][num] += score;
-                }
+                dict[st][index] += score;
             }
 
-            foreach(var v in dict)
+            foreach(var key in dict.Keys)
             {
-                char first = v.Key[0];
-                char second = v.Key[1];
+                                int first = dict[key][0];
+                int second = dict[key][1];
 
-                int firstS = v.Value[0];
-                int SecondS = v.Value[1];
-
-                answer += firstS >= SecondS ? first : second;
-
+                if (first >= second) answer += key[0];
+                else answer += key[1];
             }
 
             return answer;
