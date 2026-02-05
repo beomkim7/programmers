ecea9be7f5d89fb6a61eb615bcf80f537052d4bb
김범서 <beomkim7@naver.com>
2026-02-05 09:39:34 +0900

    [level 1] Title: 체육복, Time: 8.36 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/1/42862. 체육복/체육복.cs
+++ C#/프로그래머스/1/42862. 체육복/체육복.cs
@@ -1,28 +1,24 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
 public class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
 int answer = 0;
-
             List<int> inter = reserve.Intersect(lost).ToList();
 
             List<int> lostLst = lost.Except(inter).OrderBy(x => x).ToList();
-            List<int> reserveLst = reserve.Except(inter).OrderBy(x=>x).ToList();
-                        
+            List<int> reserveLst = reserve.Except(inter).OrderBy(x => x).ToList();
 
             for (int i = 0; i < reserveLst.Count; i++)
             {
-                int before = reserveLst[i] - 1;
-                int after = reserveLst[i] + 1;
+                int next = reserveLst[i] + 1;
+                int prev = reserveLst[i] - 1;
 
-                if (lostLst.Contains(before)) { lostLst.Remove(before); continue; }
-                else if (lostLst.Contains(after)) { lostLst.Remove(after); }
-
+                if (lostLst.Contains(prev)) { lostLst.Remove(prev); continue; }
+                else if (lostLst.Contains(next)) lostLst.Remove(next);
             }
-            answer = n - lostLst.Count();
 
+            answer = n - lostLst.Count();
             return answer;
     }
 }
