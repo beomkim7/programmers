30afbc95745ae6d321e6a09f82e3adf4464efe96
김범서 <beomkim7@naver.com>
2026-02-13 15:22:22 +0900

    [level 1] Title: 달리기 경주, Time: 325.37 ms, Memory: 187 MB -BaekjoonHub

--- C#/프로그래머스/1/178871. 달리기 경주/달리기 경주.cs
+++ C#/프로그래머스/1/178871. 달리기 경주/달리기 경주.cs
@@ -2,23 +2,27 @@ using System;
 using System.Collections.Generic;
 public class Solution {
     public string[] solution(string[] players, string[] callings) {
-string[] answer = (string[])players.Clone();
+string[] answer = new string[players.Length];
 
             Dictionary<string, int> dict = new Dictionary<string, int>();
-            for (int i = 0; i < players.Length; i++) dict[players[i]] = i;
+            for (int i = 0; i < players.Length; i++) dict.Add(players[i], i);
 
             foreach(string s in callings)
             {
                 int now = dict[s];
-                int changed = now - 1;
+                int next = dict[s] - 1;
 
-                answer[now] = answer[changed];
-                answer[changed] = s;
+                string nowS = players[now];
+                string NextS = players[next];
 
-                dict[s]--;
-                dict[answer[now]]++;
+                players[now] = players[next];
+                players[next] = nowS;
+
+                dict[nowS] = next;
+                dict[NextS] = now;
+
             }
 
-            return answer;
+            return players;
     }
 }
