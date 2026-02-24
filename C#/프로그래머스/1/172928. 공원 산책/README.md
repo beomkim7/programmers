48f31c02d5b3563fda266d6dece9a4206a285c89
김범서 <beomkim7@naver.com>
2026-02-25 08:50:40 +0900

    [level 1] Title: 공원 산책, Time: 4.09 ms, Memory: 31.8 MB -BaekjoonHub

--- C#/프로그래머스/1/172928. 공원 산책/README.md
+++ C#/프로그래머스/1/172928. 공원 산책/README.md
@@ -1,10 +1,10 @@
 # [level 1] 공원 산책 - 172928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928?language=csharp) 
 
 ### 성능 요약
 
-메모리: 31.4 MB, 시간: 1.00 ms
+메모리: 31.8 MB, 시간: 4.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 11일 11:11:05
+2026년 02월 25일 08:50:38
 
 ### 문제 설명
 
--- C#/프로그래머스/1/172928. 공원 산책/공원 산책.cs
+++ C#/프로그래머스/1/172928. 공원 산책/공원 산책.cs
@@ -4,89 +4,93 @@ public class Solution {
     public int[] solution(string[] park, string[] routes) {
 int[] answer = new int[2];
 
-            int MaxGaro = park[0].Length-1;
-            int MaxSero = park.Length-1;
+            int x = 0, y = 0;
 
-            for(int i = 0; i < park.Length; i++)
+            int maxX = park[0].Length;
+            int maxY = park.Length;
+
+            for(int i = 0; i < park.Length;i++)
             {
-                if (park[i].Contains('S'))
+                if (park[i].Contains("S")) 
                 {
-                    answer[0] = i;
-                    answer[1] = park[i].IndexOf('S');
+                    y = park[i].IndexOf("S");
+                    x = i;
                 }
             }
-
-            for(int i = 0; i < routes.Length; i++)
+            
+            foreach(string s in routes)
             {
-                string[] routeArr = routes[i].Split(' ');
-                string direction = routeArr[0];
-                int cnt = int.Parse(routeArr[1]);
+                string[] sArr = s.Split(' ');
+                string direct = sArr[0];
+                int count = int.Parse(sArr[1]);
+
+                int dx = x;
+                int dy = y;
                 bool check = true;
 
-                int nowGaro = answer[1];
-                int nowSero = answer[0];
-                switch (direction)
+                switch (direct)
                 {
                     case "E":
-                        if (nowGaro + cnt > MaxGaro) break;
-                        while (cnt > 0)
+                        for(int i = 0; i < count; i++)
                         {
-                            nowGaro++;
-                            cnt--;
-                            if (park[nowSero][nowGaro] == 'X')
+                            ++dy;
+                            if (dy < maxX && park[x][dy] != 'X') continue;
+                            else 
                             {
-                                check = false;
-                                break;
+                                dy--;
+                                check = !check;
+                                break; 
                             }
                         }
-                        if (check) answer[1] = nowGaro;
+                        if (check) y = dy;
                         break;
                     case "W":
-                        if (nowGaro - cnt < 0) break;
-                        while (cnt > 0)
+                        for (int i = 0; i < count; i++)
                         {
-                            nowGaro--;
-                            cnt--;
-                            if (park[nowSero][nowGaro] == 'X')
+                            --dy;
+                            if (dy >= 0 && park[x][dy] != 'X') continue;
+                            else
                             {
-                                check = false;
+                                dy++;
+                                check = !check;
                                 break;
                             }
                         }
-                        if (check) answer[1] = nowGaro;
+                        if (check) y = dy;
                         break;
+
                     case "S":
-                        if (nowSero + cnt > MaxSero) break;
-                        while (cnt > 0)
+                        for (int i = 0; i < count; i++)
                         {
-                            nowSero++;
-                            cnt--;
-                            if (park[nowSero][nowGaro] == 'X')
+                            ++dx;
+                            if (dx < maxY && park[dx][y] != 'X') continue;
+                            else
                             {
-                                check = false;
+                                dx--;
+                                check = !check;
                                 break;
                             }
                         }
-                        if (check) answer[0] = nowSero;
+                        if (check) x = dx;
                         break;
                     case "N":
-                        if (nowSero - cnt < 0) break;
-                        while (cnt > 0)
+                        for (int i = 0; i < count; i++)
                         {
-                            nowSero--;
-                            cnt--;
-                            if (park[nowSero][nowGaro] == 'X')
+                            --dx;
+                            if (dx >= 0 && park[dx][y] != 'X') continue;
+                            else
                             {
-                                check = false;
+                                dx++;
+                                check = !check;
                                 break;
                             }
                         }
-                        if(check)answer[0] = nowSero;
+                        if (check) x = dx;
                         break;
-
                 }
             }
-
+            answer[0] = x;
+            answer[1] = y;
             return answer;
     }
 }
